cda9132633150f4faeadea25a1ffcb5f2728df2c
Shawn Lee <shawn.lee@calmid.com>
2020-07-10 22:00:15 +0900

    cleanup 🧹

--- live/src/sendMessage.tsx
+++ live/src/sendMessage.tsx
@@ -1,61 +1,33 @@
 import React, { useState, useContext } from 'react'
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { Button, CircularProgress, Grid, useTheme, Paper, makeStyles, Theme, createStyles, TextField, Collapse } from '@material-ui/core'
+import { CircularProgress, Paper, makeStyles, Theme, createStyles, InputBase, Divider, IconButton } from '@material-ui/core'
+import SendTwoToneIcon from '@material-ui/icons/SendTwoTone';
 import { useMutation } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 import { UserContext } from './app'
 
 const useStyles = makeStyles((theme: Theme) =>
 createStyles({
-    cssFocused: {
-        "&$cssFocused": {
-            backgroundColor: "#dff0ff",
-            color: "#000", // focused
-        },
-
-    },
-    cssOutlinedInput: {
-        "&$cssFocused": {
-            borderColor: "#1896ea", // focused
-        },
-        "&:hover:not($disabled):not($cssFocused):not($error)": {
-            backgroundColor: "#b8ddff",
-            borderColor: "#7c8084", // hovered
-        },
-        "&:not(hover):not($disabled):not($cssFocused):not($error)": {
-            borderColor: "#c9caca", // default
-        },
-        "backgroundColor": "#fcfcfb",
-    },
-    liveChatInput: {
-        borderRadius: 12,
-        padding: theme.spacing(2, 4),
-        [theme.breakpoints.down("sm")]: {
-            padding: theme.spacing(2, 2),
-        },
-    },
-    paperContainer: {
-        borderRadius: 12,
-        boxShadow: theme.palette.type === "dark" ? "0px 2px 4px -1px rgba(255, 255, 255, 0.25), 0px 4px 5px 0px rgba(255, 255, 255, 0.2), 0px 1px 10px 0px rgba(255, 255, 255, 0.16)" : "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
-    },
-    smallAvatar: {
-        height: theme.spacing(4),
-        marginRight: theme.spacing(1),
-        width: theme.spacing(4),
-    },
     root: {
-        position: "fixed",
-        width: 320,
-        bottom: theme.spacing(2),
-        right: theme.spacing(1),
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 340,
+        [theme.breakpoints.down("sm")]: {
+            width: "100%",
+        },
     },
-    rootMobile: {
-        position: "fixed",
-        width: `calc(100% - ${theme.spacing(2)}px)`,
-        left: theme.spacing(1),
-        bottom: theme.spacing(2),
-        right: theme.spacing(1),
-    }
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+        color: "#000", // focused
+    },
+    iconButton: {
+        padding: 10,
+    },
+    divider: {
+        height: 28,
+        margin: 4,
+    },
 }));
 
 
@@ -69,10 +41,7 @@ const SEND_MESSAGE = gql`
 `
 export function SendMessage (): JSX.Element {
     const classes = useStyles();
-    const theme = useTheme();
-    const isSmDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const [focusedInput, setFocusedInput] = useState(false);
     const [sendMessage, { loading }] = useMutation(SEND_MESSAGE);
     const [message, setMessage] = useState('');
     const {roomId} = useContext(UserContext)
@@ -83,52 +52,28 @@ export function SendMessage (): JSX.Element {
     }
 
     return (
-        <Grid className={isSmDown ? classes.rootMobile : classes.root} item xs={12}>
-            <Paper elevation={4} className={classes.paperContainer}>
-                <Grid
-                    container
-                    direction="row"
-                    justify="space-between"
-                    alignItems="center"
-                    className={classes.liveChatInput}
-                    spacing={2}
-                >
-                    <form 
-                        onSubmit={(e) => { e.preventDefault(); send()}}
-                        style={{ flex: 1 }}
-                    >
-                        <Grid item style={{ flex: 1 }}>
-                            <TextField
-                                fullWidth
-                                id="live-chat-input"
-                                label="Share something here"
-                                variant="filled"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                onClick={() => setFocusedInput(true)}
-                                InputProps={{
-                                    classes: {
-                                        focused: classes.cssFocused,
-                                        root: classes.cssOutlinedInput,
-                                    },
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Collapse in={focusedInput}>
-                                <Grid container item xs={12} justify="flex-end" alignItems="center">
-                                    <Button
-                                        color="primary" 
-                                        onClick={() => send()}
-                                    >
-                                        {!loading ? 'Send' : <CircularProgress size={15}/>}
-                                    </Button>
-                                </Grid>
-                            </Collapse>
-                        </Grid>
-                    </form>
-                </Grid>
-            </Paper>
-        </Grid>
+        <Paper 
+            component="form"
+            className={classes.root}
+            onSubmit={(e) => { e.preventDefault(); send()}}
+        >
+            <InputBase
+                className={classes.input}
+                placeholder="Share something here"
+                inputProps={{ 'aria-label': 'input a message here' }}
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+            />
+            <Divider className={classes.divider} orientation="vertical" />
+            <IconButton 
+                aria-label="send"
+                className={classes.iconButton} 
+                color="primary"
+                onClick={() => send()}
+                type="submit"
+            >
+                {!loading ? <SendTwoToneIcon />: <CircularProgress size={12}/>}
+            </IconButton>
+        </Paper>
     )
 }
