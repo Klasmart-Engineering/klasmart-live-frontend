b7e78604f33be550892b8190d7f60a9b7a94de7c
Owen Delahoy <owen.delahoy@gmail.com>
2020-07-18 22:38:06 +0900

    Format + remove console log

--- kidsloop-live/live/src/services/event-recorder/uploader/MockEventUploader.ts
+++ kidsloop-live/live/src/services/event-recorder/uploader/MockEventUploader.ts
@@ -2,8 +2,7 @@ import { IEventUploader } from "./IEventUploader";
 import { SequencedEventData } from "../events/SequencedEvent";
 
 export class MockEventUploader implements IEventUploader {
-  upload (eventData: SequencedEventData[]) : Promise<void> {
-      console.log(eventData);
-      return Promise.resolve()
-  }
+    upload (eventData: SequencedEventData[]) : Promise<void> {
+        return Promise.resolve();
+    }
 }
