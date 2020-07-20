83f4743421e705378c7db8762714e05505f33560
owen-delahoy <owen.delahoy@calmid.com>
2020-07-20 19:24:53 +0900

    git deploy beta seoul for client

---
+++ kidsloop-live/live/deploy-beta-seoul.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+npm run build:prod
+aws s3 sync dist s3://kidsloop-live-beta/live
+aws cloudfront create-invalidation --paths /live* --distribution-id E28L3SG4NDQGZ7 #live.kidsloop.net
