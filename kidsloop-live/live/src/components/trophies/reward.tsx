dd01480f5fe5b14109d27dfda8d12a6016e15c82
Axel Wettervik <axel@calmid.com>
2020-08-12 16:53:02 +0900

    Implement initial client side effect for trophy.

---
+++ kidsloop-live/live/src/components/trophies/reward.tsx
@@ -0,0 +1,47 @@
+import React, { CSSProperties } from 'react';
+import { Transition } from 'react-transition-group';
+import { TransitionStatus } from 'react-transition-group/Transition';
+import rewardImage from '../../assets/img/trophies/star3.png';
+
+type Props = {
+    children?: React.ReactNode;
+    display?: boolean;
+    enterDuration: number;
+    enterLocation: { x: number | string, y: number | string };
+    exitLocation: { x: number | string, y: number | string };
+};
+
+export function Reward(props: Props): JSX.Element {
+    const { children, display, enterDuration, enterLocation, exitLocation } = props;
+
+    const rewardStyle: CSSProperties = {
+        position: 'absolute',
+
+        left: enterLocation.x,
+        top: enterLocation.y,
+        transform: 'translate(-50%, -50%) scale(0)',
+
+        transition: `transform ${enterDuration}ms ease-in-out, 
+                     top ${enterDuration}ms ease-in-out, 
+                     left ${enterDuration}ms ease-in-out`,
+    }
+
+    const rewardTransitionStates: Record<TransitionStatus, any> = {
+        entering: { transform: `translate(-50%, -50%) scale(0.8)`, left: '50%', top: '50%' },
+        entered: { transform: 'translate(-50%, -50%) scale(1.0)', left: '50%', top: '50%' },
+        exiting: { transform: `translate(-50%, -50%) scale(0)`, left: exitLocation.x, top: exitLocation.y },
+        exited: { transform: `translate(-50%, -50%) scale(0)`, left: enterLocation.x, top: enterLocation.y },
+        unmounted: undefined,
+    }
+
+    return (
+        <Transition in={display} timeout={enterDuration}>
+            { state => (
+                <div className="trophy-reward" style={{ ...rewardStyle, ...rewardTransitionStates[state] }}>
+                    <img alt="trophy" src={rewardImage} />
+                    {children}
+                </div>
+            )}
+        </Transition>
+    );
+}
---
+++ kidsloop-live/live/src/components/trophies/trophyTimings.ts
@@ -0,0 +1,18 @@
+
+const TIMINGS = {
+    enterDuration: 500,
+    displayDuration: 3000,
+
+    lights: {
+        enterDuration: 400,
+        displayAfter: 0,
+        displayDuration: 2000,
+    },
+    reward: {
+        enterDuration: 500,
+        displayAfter: 600,
+        displayDuration: 1500,
+    }
+}
+
+export default TIMINGS;
