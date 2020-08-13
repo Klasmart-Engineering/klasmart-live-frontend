bd43f20631cb9208ec31977be854a0789817427c
Axel Wettervik <axel@calmid.com>
2020-08-13 12:07:38 +0900

    Add trophies to the app layout.

--- live/src/app.tsx
+++ live/src/app.tsx
@@ -1,14 +1,22 @@
-import {Join} from "./pages/join";
+import { Join } from "./pages/join";
 import { Room } from "./room";
 import { UserContext } from "./entry";
 import { webRTCContext } from "./webRTCState";
 import React, { useContext } from "react";
+import { Trophy } from "./components/trophies/trophy";
+import { TrophyRewardProvider } from "./components/trophies/trophyRewardProvider";
 
-export function App (): JSX.Element {
-    const {name, teacher} = useContext(UserContext);
+export function App(): JSX.Element {
+    const { name, teacher } = useContext(UserContext);
     const webrtc = useContext(webRTCContext);
 
-    if(!name || webrtc.getCamera() === undefined) {return <Join />;}
+    if (!name || webrtc.getCamera() === undefined) { return <Join />; }
 
-    return <Room teacher={teacher} />;
+    return (
+    <TrophyRewardProvider>
+        <>
+            <Room teacher={teacher} />
+            <Trophy />
+        </>
+    </TrophyRewardProvider>);
 }
