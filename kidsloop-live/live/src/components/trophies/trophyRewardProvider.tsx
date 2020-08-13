bd7f153713ea6931e87e2b7bd4b934259e9294ae
Axel Wettervik <axel@calmid.com>
2020-08-13 10:23:59 +0900

    Add trophy kind and from fields.

--- kidsloop-live/live/src/components/trophies/trophyRewardProvider.tsx
+++ kidsloop-live/live/src/components/trophies/trophyRewardProvider.tsx
@@ -1,27 +1,42 @@
 import { useMutation, useSubscription } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
-import React, { createContext, FunctionComponent, ReactChild, ReactChildren, useCallback, useContext } from 'react';
+import React, { createContext, FunctionComponent, ReactChild, ReactChildren, useCallback, useContext, useReducer } from 'react';
 import { UserContext } from '../../entry';
 
+export type Trophy = { from: string, user: string, kind: string };
+export type TrophyHandler = (trophy: Trophy) => void;
+
+interface ITrophyState {
+    handlers: TrophyHandler[];
+}
+
+interface ITrophyActions {
+    rewardTrophy?: (user: string, kind: string) => void,
+    registerHandler?: (handler: TrophyHandler) => void,
+    removeHandler?: (handler: TrophyHandler) => void,
+}
+
 interface ITrophyContext {
-    actions: any;
+    state: ITrophyState;
+    actions: ITrophyActions;
 }
 
 const Context = createContext<ITrophyContext>({
-    actions: {},
+    state: { handlers: [] },
+    actions: {}
 });
 
 const SUBSCRIBE_ROOM_TROPHY = gql`
   subscription room($roomId: ID!, $name: String) {
     room(roomId: $roomId, name: $name) {
-        trophy { userId }
+        trophy { from, user, kind }
     }
   }
 `;
 
 const MUTATION_REWARD_TROPHY = gql`
-  mutation rewardTrophy($roomId: ID!, $userId: ID!) {
-    rewardTrophy(roomId: $roomId, userId: $userId)
+  mutation rewardTrophy($roomId: ID!, $user: ID!, $kind: String) {
+    rewardTrophy(roomId: $roomId, user: $user, kind: $kind)
   }
 `;
 
@@ -34,26 +49,52 @@ export const TrophyRewardProvider: FunctionComponent<Props> = ({ children }: Pro
 
     const [rewardTrophyMutation] = useMutation(MUTATION_REWARD_TROPHY);
 
+    const [handlers, handlersDispatch] = useReducer((state: TrophyHandler[], action: { type: 'register' | 'remove', handler: TrophyHandler }) => {
+        switch(action.type) {
+            case 'register': 
+                state.push(action.handler);
+            break;
+            case 'remove':
+                const index = state.indexOf(action.handler);
+                if (index !== -1) state.splice(index, 1);
+            break;
+            default: throw new Error('invalid action');
+        }
+
+        return state;
+    }, []);
+
     useSubscription(SUBSCRIBE_ROOM_TROPHY, {
         onSubscriptionData: ({ subscriptionData: { data: { room } } }) => {
             const { trophy } = room;
             if (trophy) {
-                console.log(trophy);
-                // TODO: Trigger trophy display.
+                handlers.forEach((handler) => {
+                    handler(trophy);
+                });
             }
         }, variables: { roomId, name }
     });
 
-    const rewardTrophy = useCallback((userId: string) => {
-        rewardTrophyMutation({ variables: { roomId, userId } });
+    const rewardTrophy = useCallback((user: string, kind: string) => {
+        rewardTrophyMutation({ variables: { roomId, user, kind } });
     }, [roomId, rewardTrophyMutation]);
 
+    const registerHandler = useCallback((handler: TrophyHandler) => {
+        handlersDispatch({ type: 'register', handler })
+    }, [handlersDispatch]);
+
+    const removeHandler = useCallback((handler: TrophyHandler) => {
+        handlersDispatch({ type: 'remove', handler })
+    }, [handlersDispatch]);
+
     const actions = {
-        rewardTrophy
+        rewardTrophy,
+        registerHandler,
+        removeHandler,
     }
 
     return (
-        <Context.Provider value={{ actions }}>
+        <Context.Provider value={{ actions, state: { handlers } }}>
             {children}
         </Context.Provider>
     )
