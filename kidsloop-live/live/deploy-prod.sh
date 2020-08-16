b706d807d0e8085982d11589a3dbe908e53f64bb
Owen Delahoy <owen.delahoy@gmail.com>
2020-08-17 00:36:31 +0900

    Update deploy script to include invalidations on kido cloudfront distribution

--- kidsloop-live/live/deploy-prod.sh
+++ kidsloop-live/live/deploy-prod.sh
@@ -2,4 +2,11 @@
 npm run build:prod
 aws s3 sync dist s3://kidsloop-live-production/live
 aws s3 sync dist s3://kidsloop-live-production/class-live
-aws cloudfront create-invalidation --paths /class-live* --distribution-id E2FR6R4F7UAK0I #demo-school.kidsloop.net demo-home.kidsloop.net
+# demo-school.kidsloop.net
+# demo-home.kidsloop.net
+aws cloudfront create-invalidation --paths /class-live* --distribution-id E2FR6R4F7UAK0I
+# ppe-school.kidsloop.net
+# ppe-home.kidsloop.net
+# kido-school.kidsloop.net
+# kido-home.kidsloop.net
+aws cloudfront create-invalidation --paths /class-live* --distribution-id E1LJ2X1AEAH8EV
