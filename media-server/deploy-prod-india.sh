477f21e69b2b6d169e25a35a5386227daf8cb5e9
Owen Delahoy <owen.delahoy@gmail.com>
2020-09-08 23:20:46 +0900

    Don't trigger ECS deployment when deploying SFU

--- media-server/deploy-prod-india.sh
+++ media-server/deploy-prod-india.sh
@@ -3,4 +3,4 @@ aws ecr get-login-password --region ap-south-1 | docker login --username AWS --p
 docker build -t prod-sfu .
 docker tag prod-sfu:latest 494634321140.dkr.ecr.ap-south-1.amazonaws.com/prod-sfu:latest
 docker push 494634321140.dkr.ecr.ap-south-1.amazonaws.com/prod-sfu:latest
-aws ecs update-service --service arn:aws:ecs:ap-south-1:494634321140:service/prod-hub/prod-sfu-service --force-new-deployment --cluster prod-hub --region ap-south-1
+# aws ecs update-service --service arn:aws:ecs:ap-south-1:494634321140:service/prod-hub/prod-sfu-service --force-new-deployment --cluster prod-hub --region ap-south-1
