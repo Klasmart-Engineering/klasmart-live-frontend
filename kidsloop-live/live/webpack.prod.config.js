d5586aeeefa79bfa70cbb7ff7552de11429ce003
Axel Wettervik <axel.wettervik@gmail.com>
2020-09-10 18:50:17 +0900

    Reset webpack configuration changes.

--- kidsloop-live/live/webpack.prod.config.js
+++ kidsloop-live/live/webpack.prod.config.js
@@ -1,47 +1,47 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const SentryWebpackPlugin = require("@sentry/webpack-plugin")
 
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    ui: "./src/entry.tsx",
-    record: "./src/entry-record.ts",
-    player: "./src/entry-player.ts",
+    ui: './src/entry.tsx',
+    record: './src/entry-record.ts',
+    player: './src/entry-player.ts'
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.tsx?$/i,
         exclude: /node_modules/,
         use: {
-          loader: "ts-loader",
-        },
+          loader: 'ts-loader'
+        }
       },
       {
         test: /\.css$/i,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader'
           },
-          "css-modules-typescript-loader",
+          'css-modules-typescript-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              modules: true,
-            },
-          },
-        ],
+              modules: true
+            }
+          }
+        ]
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          "file-loader",
+          'file-loader',
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
               // mozjpeg: {
               //     progressive: true,
@@ -52,56 +52,51 @@ module.exports = {
               //     enabled: false,
               // },
               pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
+                quality: [0.65, 0.90],
+                speed: 4
               },
-            },
-          },
+            }
+          }
         ],
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf|TTF)$/,
-        use: ["file-loader"],
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          "file-loader",
+        ],
       },
       {
         test: /\.mp3$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         query: {
-          name: "static/media/[name].[hash:8].[ext]",
-        },
-      },
-    ],
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: [".js", ".jsx", ".tsx", ".ts"],
+    extensions: ['.js', '.jsx', '.tsx', '.ts']
   },
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new webpack.EnvironmentPlugin(
-      ["NODE_ENV"],
-      ["ENDPOINT_GQL"],
-      ["ENDPOINT_H5P"],
-      ["ENDPOINT_TEST_ASSETS_S3"],
-      ["ENDPOINT_WEBSOCKET"],
-      ["APP_GIT_REV"]
-    ),
+    new webpack.EnvironmentPlugin(['NODE_ENV'], ['ENDPOINT_GQL'], ['ENDPOINT_H5P'], ['ENDPOINT_TEST_ASSETS_S3'], ['ENDPOINT_WEBSOCKET'], ['APP_GIT_REV']),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      chunks: ["ui"],
-      template: "src/index.html",
+      filename: 'index.html',
+      chunks: ['ui'],
+      template: 'src/index.html'
     }),
     new HtmlWebpackPlugin({
-      filename: "player.html",
-      chunks: ["player"],
-      template: "src/player.html",
+      filename: 'player.html',
+      chunks: ['player'],
+      template: 'src/player.html'
     }),
     new SentryWebpackPlugin({
       include: ".",
       ignoreFile: ".sentrycliignore",
       ignore: ["node_modules", "webpack.config.js", "webpack.prod.config.js"],
     }),
-  ],
-};
+  ]
+}
