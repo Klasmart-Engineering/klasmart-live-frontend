61408f86003c759caf9457d2c589ec28ee18ed8d
Isu Ahn <isu.ahn@calmid.com>
2020-09-17 12:23:22 +0900

    Accept token when searchParam is existed in live.kidsloop.net

--- kidsloop-live/live/src/entry.tsx
+++ kidsloop-live/live/src/entry.tsx
@@ -86,26 +86,37 @@ if (url.hostname !== "localhost" && url.hostname !== "live.kidsloop.net") {
 function parseToken() {
     try {
         if (url.hostname === "localhost" || url.hostname === "live.kidsloop.net" || url.hostname.includes("ngrok.io")) {
-            const materialsParam = url.searchParams.get("materials");
-            return {
-                teacher: url.searchParams.get("teacher") !== null,
-                name: url.searchParams.get("name") || undefined, // Should be undefined not null
-                roomId: url.searchParams.get("roomId") || "test-room",
-                materials: materialsParam ? JSON.parse(materialsParam) : [
-                    { __typename: MaterialTypename.Iframe, name: "Pairs", url: "/h5p/play/5ecf4e4b611e18398f7380ef" },
-                    { __typename: MaterialTypename.Iframe, name: "Flashcards", url: "/h5p/play/5ed05dd1611e18398f7380f4" },
-                    { __typename: MaterialTypename.Iframe, name: "Drag and Drop", url: "/h5p/play/5ed0b64a611e18398f7380fb" },
-                    { __typename: MaterialTypename.Iframe, name: "Hot Spot 1", url: "/h5p/play/5ecf6f43611e18398f7380f0" },
-                    { __typename: MaterialTypename.Iframe, name: "Hot Spot 2", url: "/h5p/play/5ed0a79d611e18398f7380f7" },
-                    { __typename: MaterialTypename.Video, name: "Video", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
-                    { __typename: MaterialTypename.Audio, name: "Audio", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_audio.m4a` },
-                    { __typename: MaterialTypename.Image, name: "Portrait Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_portrait.jpg` },
-                    { __typename: MaterialTypename.Image, name: "Landscape Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_landscape.jpg` },
-                    { name: "Pairs - Legacy", url: `/h5p/play/5ecf4e4b611e18398f7380ef` },
-                    { name: "Video - Legacy", video: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
-                    { __typename: MaterialTypename.Iframe, name: "Quiz", url: "/h5p/play/5ed07656611e18398f7380f6" },
-                ],
-            };
+            const token = url.searchParams.get("token");
+            if (token) {
+                const payload = jwt_decode(token) as any;
+                return {
+                    teacher: payload.teacher ? Boolean(payload.teacher) : false,
+                    name: payload.name ? String(payload.name) : undefined,
+                    roomId: String(payload.roomid),
+                    materials: payload.materials || [],
+                };
+            } else {
+                const materialsParam = url.searchParams.get("materials");
+                return {
+                    teacher: url.searchParams.get("teacher") !== null,
+                    name: url.searchParams.get("name") || undefined, // Should be undefined not null
+                    roomId: url.searchParams.get("roomId") || "test-room",
+                    materials: materialsParam ? JSON.parse(materialsParam) : [
+                        { __typename: MaterialTypename.Iframe, name: "Pairs", url: "/h5p/play/5ecf4e4b611e18398f7380ef" },
+                        { __typename: MaterialTypename.Iframe, name: "Flashcards", url: "/h5p/play/5ed05dd1611e18398f7380f4" },
+                        { __typename: MaterialTypename.Iframe, name: "Drag and Drop", url: "/h5p/play/5ed0b64a611e18398f7380fb" },
+                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 1", url: "/h5p/play/5ecf6f43611e18398f7380f0" },
+                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 2", url: "/h5p/play/5ed0a79d611e18398f7380f7" },
+                        { __typename: MaterialTypename.Video, name: "Video", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
+                        { __typename: MaterialTypename.Audio, name: "Audio", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_audio.m4a` },
+                        { __typename: MaterialTypename.Image, name: "Portrait Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_portrait.jpg` },
+                        { __typename: MaterialTypename.Image, name: "Landscape Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_landscape.jpg` },
+                        { name: "Pairs - Legacy", url: `/h5p/play/5ecf4e4b611e18398f7380ef` },
+                        { name: "Video - Legacy", video: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
+                        { __typename: MaterialTypename.Iframe, name: "Quiz", url: "/h5p/play/5ed07656611e18398f7380f6" },
+                    ],
+                };
+            }
         }
 
         const token = url.searchParams.get("token");
@@ -124,7 +135,7 @@ function parseToken() {
 const params = parseToken();
 function Entry() {
     if (!params) { return <Typography><FormattedMessage id="error_invaild_token" /></Typography>; }
-    const [camera, setCamera] = useState<MediaStream|null>();
+    const [camera, setCamera] = useState<MediaStream | null>();
     const [name, setName] = useState(params.name);
     const [languageCode, setLanguageCode] = useState(url.searchParams.get("lang") || getDefaultLanguageCode());
     const [themeMode, setThemeMode] = useState(url.searchParams.get("theme") || "light");
