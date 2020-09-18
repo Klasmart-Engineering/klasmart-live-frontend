99b4f8e32adbd01faa43d28ce901feefc4a6bb5f
owen-delahoy <owen.delahoy@calmid.com>
2020-09-18 17:11:47 +0900

    Invalidate both paths

--- kidsloop-live/live/deploy-beta-seoul.sh
+++ kidsloop-live/live/deploy-beta-seoul.sh
@@ -3,4 +3,5 @@ npm i
 npm run build:prod
 aws s3 sync dist s3://kidsloop-live-beta/class-live
 aws s3 sync dist s3://kidsloop-live-beta/live
+aws cloudfront create-invalidation --paths /class-live* --distribution-id E28L3SG4NDQGZ7 #live.kidsloop.net
 aws cloudfront create-invalidation --paths /live* --distribution-id E28L3SG4NDQGZ7 #live.kidsloop.net
