35538014efa68b5277ee168de0b2a57f22c82657
Axel Wettervik <axel.wettervik@gmail.com>
2020-10-15 14:53:47 +0900

    Create absolute URL's for content.

--- kidsloop-live/live/src/entry.tsx
+++ kidsloop-live/live/src/entry.tsx
@@ -113,18 +113,17 @@ function parseToken() {
                 };
             } else {
                 const materialsParam = url.searchParams.get("materials");
-                const h5p = process.env.ENDPOINT_H5P || "/h5p";
 
                 return {
                     teacher: url.searchParams.get("teacher") !== null,
                     name: url.searchParams.get("name") || undefined, // Should be undefined not null
-                    roomId: url.searchParams.get("roomId") || "app-room",
+                    roomId: url.searchParams.get("roomId") || "axel",
                     materials: materialsParam ? JSON.parse(materialsParam) : [
-                        { __typename: MaterialTypename.Iframe, name: "Pairs", url: `${h5p}/play/5ecf4e4b611e18398f7380ef` },
-                        { __typename: MaterialTypename.Iframe, name: "Flashcards", url: `${h5p}/play/5ed05dd1611e18398f7380f4` },
-                        { __typename: MaterialTypename.Iframe, name: "Drag and Drop", url: `${h5p}/play/5ed0b64a611e18398f7380fb` },
-                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 1", url: `${h5p}/play/5ecf6f43611e18398f7380f0` },
-                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 2", url: `${h5p}/play/5ed0a79d611e18398f7380f7` },
+                        { __typename: MaterialTypename.Iframe, name: "Pairs", url: `/h5p/play/5ecf4e4b611e18398f7380ef` },
+                        { __typename: MaterialTypename.Iframe, name: "Flashcards", url: `/h5p/play/5ed05dd1611e18398f7380f4` },
+                        { __typename: MaterialTypename.Iframe, name: "Drag and Drop", url: `/h5p/play/5ed0b64a611e18398f7380fb` },
+                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 1", url: `/h5p/play/5ecf6f43611e18398f7380f0` },
+                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 2", url: `/h5p/play/5ed0a79d611e18398f7380f7` },
                         { __typename: MaterialTypename.Video, name: "Video", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
                         { __typename: MaterialTypename.Audio, name: "Audio", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_audio.m4a` },
                         { __typename: MaterialTypename.Image, name: "Portrait Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_portrait.jpg` },
