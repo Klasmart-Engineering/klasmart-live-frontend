8a942c60582c8f6456eaf3e2642238ed01d4913b
Axel Wettervik <axel.wettervik@gmail.com>
2020-10-21 20:58:48 +0900

    Add script to do development build.

---
+++ live/build-android-development.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Configure Environment
+export ENDPOINT_SFU=wss://live.kidsloop.net/sfu
+export ENDPOINT_WEBSOCKET=wss://live.kidsloop.net/graphql
+export ENDPOINT_GQL=https://live.kidsloop.net/graphql
+export ENDPOINT_CONTENT=https://live.kidsloop.net/
+export DISABLE_BROWSER_GUIDE=1
+export DISABLE_SCREEN_SHARE=1
+export USE_TEST_TOKEN=1
+
+# Clean Build
+rm -rf ./www
+rm -rf ./platforms/android
+
+# Ensure packages is installed
+npm install
+
+# Package using webpack
+npm run build:app
+
+# Prepare android platform
+cordova prepare android
+
+# Build apk
+cordova build android -- --buildConfig
