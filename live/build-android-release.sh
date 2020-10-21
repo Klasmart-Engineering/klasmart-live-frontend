27b3237def3cb697fd9c70b344c915a3a43e470f
Axel Wettervik <axel.wettervik@gmail.com>
2020-10-21 20:46:28 +0900

    Clean build to reduce size, also making AAB.

--- live/build-android-release.sh
+++ live/build-android-release.sh
@@ -9,14 +9,18 @@ export DISABLE_BROWSER_GUIDE=1
 export DISABLE_SCREEN_SHARE=1
 export USE_TEST_TOKEN=1
 
+# Clean Build
+rm -rf ./www
+rm -rf ./platforms/android
+
+# Ensure packages is installed
+npm install
+
 # Package using webpack
 npm run build:app:release
-
-# Clean Build
-rm -rf ./platforms/android
 
 # Prepare android platform
 cordova prepare android
 
 # Build apk
-cordova build android --release -- --keystore=./keystore/kidsloop-platform.keystore --storePassword=kidsloop --alias=kidsloop_platform_production --password=kidsloop
+cordova build android --release -- --buildConfig
