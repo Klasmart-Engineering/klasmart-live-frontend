9461c839201f3b070d2b8e692a75edfdddd0286c
Axel Wettervik <axel.wettervik@gmail.com>
2020-11-12 16:27:13 +0900

    Implement reducers for manipulating history state.

--- kidsloop-live/live/src/store/reducers/location.ts
+++ kidsloop-live/live/src/store/reducers/location.ts
@@ -20,13 +20,45 @@ const locationSlice = createSlice({
         },
         setHistory(state, action) {
             return { ...state, history: action.payload }
+        },
+        pushHistory(state, action) {
+            let newHistory = Array.from(state.history);
+            newHistory.push(action.payload);
+
+            return { ...state, history: newHistory }
+        },
+        pushReplaceHistory(state, action) {
+            let newHistory = [];
+            for (let i = 0; i < state.history.length; ++i) {
+                if (state.history[i] === action.payload) {
+                    break;
+                }
+
+                newHistory.push(state.history[i]);
+            }
+
+            newHistory.push(action.payload);
+
+            return { ...state, history: newHistory }
+        },
+        popHistory(state, _) {
+            if (state.history.length > 0) {
+                let newHistory = state.history.slice(0, state.history.length - 1);
+
+                return { ...state, history: newHistory }
+            } else {
+                return state;
+            }
         }
     }
 })
 
 export const {
     setDeviceOrientation,
-    setHistory
+    setHistory,
+    pushHistory,
+    pushReplaceHistory,
+    popHistory,
 } = locationSlice.actions
 
 export default locationSlice.reducer
