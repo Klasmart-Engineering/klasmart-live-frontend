0af4db40b732fb280ebf0b2013c630740ca6b28f
Owen Delahoy <owen.delahoy@gmail.com>
2020-12-14 16:32:04 +0900

    Remove old test code and domain specific behavior

--- src/entry.tsx
+++ src/entry.tsx
@@ -100,53 +100,29 @@ if (url.hostname !== "localhost" && url.hostname !== "live.beta.kidsloop.net") {
 
 function parseToken() {
     try {
-        if (url.hostname === "localhost" || url.hostname === "live.kidsloop.net" || url.hostname.includes("ngrok.io")) {
-            const token = url.searchParams.get("token");
-            if (token) {
-                const payload = jwt_decode(token) as any;
-                const materials = payload.materials ? payload.materials : []
-                const parsedMaterials = materials.map((mat: any) => {
-                    if (mat.__typename === "Iframe") {
-                        return { __typename: MaterialTypename.Iframe, name: mat.name, url: mat.url };
-                    } else if (mat.__typename === "Video") {
-                        return { __typename: MaterialTypename.Video, name: mat.name, url: mat.url };
-                    } else if (mat.__typename === "Audio") {
-                        return { __typename: MaterialTypename.Audio, name: mat.name, url: mat.url };
-                    } else if (mat.__typename === "Image") {
-                        return { __typename: MaterialTypename.Image, name: mat.name, url: mat.url };
-                    }
-                });
-                return {
-                    classtype: payload.classtype ? String(payload.classtype) : "live",
-                    org_id: payload.org_id ? String(payload.org_id) : "",
-                    teacher: payload.teacher ? Boolean(payload.teacher) : false,
-                    name: payload.name ? String(payload.name) : undefined,
-                    roomId: String(payload.roomid),
-                    materials: parsedMaterials || [],
-                };
-            } else {
-                const materialsParam = url.searchParams.get("materials");
-                return {
-                    classtype: url.searchParams.get("classtype") || "live",
-                    org_id: url.searchParams.get("org_id") || "",
-                    teacher: url.searchParams.get("teacher") !== null,
-                    name: url.searchParams.get("name") || undefined, // Should be undefined not null
-                    roomId: url.searchParams.get("roomId") || "test-room",
-                    materials: materialsParam ? JSON.parse(materialsParam) : [
-                        { __typename: MaterialTypename.Iframe, name: "Pairs", url: "/h5p/play/5ecf4e4b611e18398f7380ef" },
-                        { __typename: MaterialTypename.Iframe, name: "Flashcards", url: "/h5p/play/5ed05dd1611e18398f7380f4" },
-                        { __typename: MaterialTypename.Iframe, name: "Drag and Drop", url: "/h5p/play/5ed0b64a611e18398f7380fb" },
-                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 1", url: "/h5p/play/5ecf6f43611e18398f7380f0" },
-                        { __typename: MaterialTypename.Iframe, name: "Hot Spot 2", url: "/h5p/play/5ed0a79d611e18398f7380f7" },
-                        { __typename: MaterialTypename.Video, name: "Video", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
-                        { __typename: MaterialTypename.Audio, name: "Audio", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_audio.m4a` },
-                        { __typename: MaterialTypename.Image, name: "Portrait Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_portrait.jpg` },
-                        { __typename: MaterialTypename.Image, name: "Landscape Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_landscape.jpg` },
-                        { name: "Pairs - Legacy", url: `/h5p/play/5ecf4e4b611e18398f7380ef` },
-                        { name: "Video - Legacy", video: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
-                    ],
-                };
-            }
+        const token = url.searchParams.get("token");
+        if (token) {
+            const payload = jwt_decode(token) as any;
+            const materials = payload.materials ? payload.materials : []
+            const parsedMaterials = materials.map((mat: any) => {
+                if (mat.__typename === "Iframe") {
+                    return { __typename: MaterialTypename.Iframe, name: mat.name, url: mat.url };
+                } else if (mat.__typename === "Video") {
+                    return { __typename: MaterialTypename.Video, name: mat.name, url: mat.url };
+                } else if (mat.__typename === "Audio") {
+                    return { __typename: MaterialTypename.Audio, name: mat.name, url: mat.url };
+                } else if (mat.__typename === "Image") {
+                    return { __typename: MaterialTypename.Image, name: mat.name, url: mat.url };
+                }
+            });
+            return {
+                classtype: payload.classtype ? String(payload.classtype) : "live",
+                org_id: payload.org_id ? String(payload.org_id) : "",
+                teacher: payload.teacher ? Boolean(payload.teacher) : false,
+                name: payload.name ? String(payload.name) : undefined,
+                roomId: String(payload.roomid),
+                materials: parsedMaterials || [],
+            };
         }
     } catch (e) { }
     return;
