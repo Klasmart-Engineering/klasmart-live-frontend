6c85cad716f384ad9e3cf426e7d983eab9c0f413
Daiki Nakashita <daikinakashita@Daikiui-MacBookPro.local>
2021-02-26 14:30:41 +0900

    teachers can see students' entire activities regardless of each student's screen size

--- src/utils/viewport.ts
+++ src/utils/viewport.ts
@@ -18,3 +18,24 @@ export function isElementInViewport(ref: React.RefObject<HTMLElement>): boolean 
   
     return isIntersecting
   }
+
+  export function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+  
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+    }, []); 
+  
+    return windowSize;
+  }
