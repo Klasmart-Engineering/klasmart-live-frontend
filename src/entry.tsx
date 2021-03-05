7b2ad8d07bc760321c6f52487a2aa650c3be2269
Daiki Nakashita <daikinakashita@Daikiui-MacBookPro.local>
2021-03-05 11:16:49 +0900

    add environmental variables for debugging

--- src/entry.tsx
+++ src/entry.tsx
@@ -100,6 +100,32 @@ if (url.hostname !== "localhost" && url.hostname !== "live.beta.kidsloop.net") {
 
 function parseToken() {
     try {
+        // TODO think of a better way to set up the debug environment
+        const isDebugMode = url.hostname === "localhost" || url.hostname === "0.0.0.0";
+        if (isDebugMode) {
+            const materialsParam = url.searchParams.get("materials");
+            return {
+                classtype: url.searchParams.get("classtype") || "live",
+                org_id: url.searchParams.get("org_id") || "",
+                teacher: url.searchParams.get("teacher") !== null,
+                name: url.searchParams.get("name") || undefined, // Should be undefined not null
+                roomId: url.searchParams.get("roomId") || "test-room",
+                materials: materialsParam ? JSON.parse(materialsParam) : [
+                    { __typename: MaterialTypename.Iframe, name: "Pairs", url: "/h5p/play/5ecf4e4b611e18398f7380ef" },
+                    { __typename: MaterialTypename.Iframe, name: "Flashcards", url: "/h5p/play/5ed05dd1611e18398f7380f4" },
+                    { __typename: MaterialTypename.Iframe, name: "Drag and Drop", url: "/h5p/play/5ed0b64a611e18398f7380fb" },
+                    { __typename: MaterialTypename.Iframe, name: "Hot Spot 1", url: "/h5p/play/5ecf6f43611e18398f7380f0" },
+                    { __typename: MaterialTypename.Iframe, name: "Hot Spot 2", url: "/h5p/play/5ed0a79d611e18398f7380f7" },
+                    { __typename: MaterialTypename.Video, name: "Video", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
+                    { __typename: MaterialTypename.Audio, name: "Audio", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_audio.m4a` },
+                    { __typename: MaterialTypename.Image, name: "Portrait Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_portrait.jpg` },
+                    { __typename: MaterialTypename.Image, name: "Landscape Image", url: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_image_landscape.jpg` },
+                    { name: "Pairs - Legacy", url: `/h5p/play/5ecf4e4b611e18398f7380ef` },
+                    { name: "Video - Legacy", video: `${process.env.ENDPOINT_TEST_ASSETS_S3 || "."}/test_video.mp4` },
+                ],
+            };
+        }
+
         const token = url.searchParams.get("token");
         if (token) {
             const payload = jwt_decode(token) as any;
