01cbc590b01f231b0862049fad4b03f2dc2fb646
Gael <gael.helle@calmid.com>
2021-04-16 15:33:28 +0900

    Improve end class / feedback logic and styles

--- src/newuiupdate/layout.tsx
+++ src/newuiupdate/layout.tsx
@@ -1,30 +1,19 @@
-import Main from './components/main';
-import Sidebar from './components/sidebar';
-import { Grid } from '@material-ui/core';
 import React from 'react';
 
 import { classEndedState } from "./states/layoutAtoms";
 import { useRecoilState } from "recoil";
-import ClassEnded from './components/others/classEnded';
+import ClassEnded from './components/others/endPage/classEnded';
+import Class from './pages/class';
 
 function Layout () {
     const [ classEnded, setClassEnded ] = useRecoilState(classEndedState);
 
-    if(classEnded){
+    if(!classEnded){
         return(<ClassEnded />)
     }
 
     return (
-        <Grid container>
-            <Grid
-                item
-                xs>
-                <Main />
-            </Grid>
-            <Grid item>
-                <Sidebar />
-            </Grid>
-        </Grid>
+        <Class />
     );
 }
 
---
+++ src/newuiupdate/pages/class.tsx
@@ -0,0 +1,21 @@
+import Main from '../components/main';
+import Sidebar from '../components/sidebar';
+import { Grid } from '@material-ui/core';
+import React from 'react';
+
+function Class () {
+    return (
+        <Grid container>
+            <Grid
+                item
+                xs>
+                <Main />
+            </Grid>
+            <Grid item>
+                <Sidebar />
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Class;
