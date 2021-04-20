648bd48b5d5bfe066a779392335a7b21bb449f03
Gael <gael.helle@calmid.com>
2021-04-20 14:11:24 +0900

    add tooltip global actions menu

--- src/newuiupdate/components/main/pinUser/User.tsx
+++ src/newuiupdate/components/main/pinUser/User.tsx
@@ -1,58 +1,54 @@
-import React, { useState } from "react";
-
-import { makeStyles, Box, Grid, Theme, Typography } from "@material-ui/core";
-
-import clsx from "clsx";
-
-import { UserAvatar } from "kidsloop-px";
-
-
-
-import { Pin as PinIcon } from "@styled-icons/entypo/Pin";
-
-import { useRecoilState } from "recoil";
 import { pinnedUserState } from "../../../states/layoutAtoms";
-
+import {
+    Grid, makeStyles, Theme, Typography,
+} from "@material-ui/core";
 import amber from "@material-ui/core/colors/amber";
+import { Pin as PinIcon } from "@styled-icons/entypo/Pin";
+import clsx from "clsx";
+import { UserAvatar } from "kidsloop-px";
+import React from "react";
+import { useRecoilState } from "recoil";
 
 const useStyles = makeStyles((theme: Theme) => ({
     root:{
-        cursor: 'pointer',
+        cursor: `pointer`,
         borderRadius: 12,
         flexGrow: 1,
         marginTop: 2,
         marginBottom: 2,
         "&:hover":{
-            backgroundColor: '#fff',
-            "& $iconButton":{
-                visibility: 'visible',
-            }
+            backgroundColor: `#fff`,
         },
     },
-    active:{
-        
-    },
+    active:{},
     avatar:{
-        marginRight: 5
+        marginRight: 5,
     },
     pinIcon:{
-        color: amber[500]
-    }
+        color: amber[500],
+    },
 }));
 
 export interface UserProps {
-    id: number,
-    name: string,
+    id: number;
+    name: string;
 }
 
-function User(props: UserProps) {
-    const {id, name} = props;
-	const classes = useStyles();
+function User (props: UserProps) {
+    const { id, name } = props;
+    const classes = useStyles();
 
-    const [pinnedUser, setPinnedUser] = useRecoilState(pinnedUserState);
+    const [ pinnedUser, setPinnedUser ] = useRecoilState(pinnedUserState);
 
-	return (
-		<Grid container alignItems="center" spacing={2} className={clsx(classes.root, {[classes.active] : id === pinnedUser})} onClick={() => id === pinnedUser ? setPinnedUser(undefined) : setPinnedUser(id)}>
+    return (
+        <Grid
+            container
+            alignItems="center"
+            spacing={2}
+            className={clsx(classes.root, {
+                [classes.active] : id === pinnedUser,
+            })}
+            onClick={() => id === pinnedUser ? setPinnedUser(undefined) : setPinnedUser(id)}>
             <Grid item>
                 <UserAvatar
                     name={name}
@@ -60,17 +56,21 @@ function User(props: UserProps) {
                     size="medium"
                 />
             </Grid>
-            <Grid item xs>
+            <Grid
+                item
+                xs>
                 <Typography>{name}</Typography>
             </Grid>
 
-            {id === pinnedUser && 
-                <Grid item className={classes.pinIcon}>
+            {id === pinnedUser &&
+                <Grid
+                    item
+                    className={classes.pinIcon}>
                     <PinIcon size="1.5rem"/>
                 </Grid>
             }
         </Grid>
-	);
+    );
 }
 
 export default User;
--- src/newuiupdate/components/toolbar/toolbarMenus/globalActionsMenu/item.tsx
+++ src/newuiupdate/components/toolbar/toolbarMenus/globalActionsMenu/item.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-
-import { makeStyles, Grid, Theme, Box } from "@material-ui/core";
-
-import red from "@material-ui/core/colors/red";
+import {
+    Grid, makeStyles, Theme, Tooltip,
+} from "@material-ui/core";
 import amber from "@material-ui/core/colors/amber";
-
+import red from "@material-ui/core/colors/red";
 import clsx from "clsx";
+import React from "react";
 
 interface GlobaActionsMenuProps {
 	type?: any;
@@ -18,71 +17,77 @@ interface GlobaActionsMenuProps {
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
-	root: {
-		padding: "8px 16px",
-		margin: "0 4px",
-		cursor: "pointer",
-		borderRadius: 10,
-		display: "flex",
-		color: amber[500],
-		filter: "drop-shadow( 1px 3px 2px rgba(255, 193,20, .2))",
-		transition: "100ms all ease-in-out",
-		"&:hover": {
-			backgroundColor: theme.palette.grey[200],
-			filter: "none",
-		},
-	},
-	active: {
-		backgroundColor: theme.palette.text.primary,
-		filter: "none",
-		color: '#fff',
-		"&:hover": {
-			backgroundColor: theme.palette.text.primary,
-		},
-	},
-	variantBlue:{
-		color: theme.palette.text.primary,
-		filter: "drop-shadow( 1px 3px 2px rgba(82, 128, 191, .2))",
-	},
-	variantRed:{
-		color: red[500],
-		filter: "drop-shadow( 1px 3px 2px rgba(255, 116, 106, .2))",
-	},
-	divider: {
-		width: 1,
-		backgroundColor: theme.palette.grey[200],
-		margin: "0 4px",
-	},
-	icon: {
-		display: "flex",
-		alignItems: "center",
-	},
+    root: {
+        padding: `8px 16px`,
+        margin: `0 4px`,
+        cursor: `pointer`,
+        borderRadius: 10,
+        display: `flex`,
+        color: amber[500],
+        filter: `drop-shadow( 1px 3px 2px rgba(255, 193,20, .2))`,
+        transition: `100ms all ease-in-out`,
+        "&:hover": {
+            backgroundColor: theme.palette.grey[200],
+            filter: `none`,
+        },
+    },
+    active: {
+        backgroundColor: theme.palette.text.primary,
+        filter: `none`,
+        color: `#fff`,
+        "&:hover": {
+            backgroundColor: theme.palette.text.primary,
+        },
+    },
+    variantBlue:{
+        color: theme.palette.text.primary,
+        filter: `drop-shadow( 1px 3px 2px rgba(82, 128, 191, .2))`,
+    },
+    variantRed:{
+        color: red[500],
+        filter: `drop-shadow( 1px 3px 2px rgba(255, 116, 106, .2))`,
+    },
+    divider: {
+        width: 1,
+        backgroundColor: theme.palette.grey[200],
+        margin: `0 4px`,
+    },
+    icon: {
+        display: `flex`,
+        alignItems: `center`,
+    },
 }));
 
-function GlobalActionsMenuItem(props: GlobaActionsMenuProps) {
-	const { type, icon, title, variant, active, activeIcon, onClick } = props;
+function GlobalActionsMenuItem (props: GlobaActionsMenuProps) {
+    const {
+        type, icon, title, variant, active, activeIcon, onClick,
+    } = props;
 
-	const classes = useStyles();
+    const classes = useStyles();
 
-	if (type === "divider") return <Grid item className={classes.divider}></Grid>;
+    if (type === `divider`) return <Grid
+        item
+        className={classes.divider}></Grid>;
 
-	return (
-		<Grid
-			item
-			className={clsx(
-				classes.root, {
-				[classes.active]:  active,
-				[classes.variantBlue]:  variant === 'blue' && !active,
-				[classes.variantRed]:  variant === 'red' && !active
-				}
-			)}
-			onClick={onClick}
-		>
-			<div className={classes.icon}>
-				{activeIcon ? active ? activeIcon : icon: icon}
-			</div>
-		</Grid>
-	);
+    return (
+        <Tooltip
+            title={title}
+            placement="top">
+            <Grid
+                item
+                className={clsx(classes.root, {
+                    [classes.active]:  active,
+                    [classes.variantBlue]:  variant === `blue` && !active,
+                    [classes.variantRed]:  variant === `red` && !active,
+                })}
+                onClick={onClick}
+            >
+                <div className={classes.icon}>
+                    {activeIcon ? active ? activeIcon : icon: icon}
+                </div>
+            </Grid>
+        </Tooltip>
+    );
 }
 
 export default GlobalActionsMenuItem;
