1de39d33376470f04eedbe9dad4f9f41fe5a5402
Phong Tran <phong.tran@calmid.com>
2021-05-06 10:44:50 +0000

    Merged in VNSRE-56-change-vietnam-env (pull request #123)
    VNSRE-56 change vietnam env
    
    * VNSRE-56 change vietnam env
    
    * Add webpack config for beta environment
    
    * Add build:beta command
    
    Approved-by: Guy Richardson

--- deploy/config/vn/webpack.alpha.config.js
+++ deploy/config/vn/webpack.alpha.config.js
@@ -84,7 +84,7 @@ module.exports = {
   plugins: [
     new webpack.EnvironmentPlugin(
       { "CALLSTATS_ENABLE": "TRUE" },
-      { "ENDPOINT_CMS": "https://cms.kidsloop.vn" },
+      { "ENDPOINT_CMS": "https://cms.alpha.kidsloop.vn" },
     ),
     new HtmlWebpackPlugin({
       filename: 'index.html',
