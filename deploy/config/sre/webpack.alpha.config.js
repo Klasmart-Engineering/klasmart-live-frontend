cafb14b756f167892f3aee8a43c354e47a5cfc7a
Seb Margineanu <sebastian.margineanu@opencredo.com>
2021-05-13 14:45:29 +0100

    update config with .alpha. in domain name

--- deploy/config/sre/webpack.alpha.config.js
+++ deploy/config/sre/webpack.alpha.config.js
@@ -84,7 +84,7 @@ module.exports = {
   plugins: [
     new webpack.EnvironmentPlugin(
       { "CALLSTATS_ENABLE": "TRUE" },
-      { "ENDPOINT_CMS": "https://cms.klpsre.com" },
+      { "ENDPOINT_CMS": "https://cms.alpha.klpsre.com" },
     ),
     new HtmlWebpackPlugin({
       filename: 'index.html',
