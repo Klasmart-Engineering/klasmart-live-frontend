2ac04fd4be342420e7154799d65cab5122806aa9
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-05-27 15:19:12 +0900

    Updates record hashes to force cache busting

--- webpack.alpha.config.js
+++ webpack.alpha.config.js
@@ -9,7 +9,7 @@ module.exports = {
   mode: 'production',
   entry: {
     ui: './src/entry.tsx',
-    "record-e44f2b3": './src/entry-record.ts',
+    "record-1db5341": './src/entry-record.ts',
     player: './src/entry-player.ts'
   },
   module: {
