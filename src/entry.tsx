d17db5c772454325d38609b3bc329bd8378c8999
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-05-28 17:43:15 +0900

    Reverts local testing configuration changes in entry.tsx, removes minor addition that added a code smell to providers.tsx

--- src/entry.tsx
+++ src/entry.tsx
@@ -185,26 +185,10 @@ function parseToken () {
             return {
                 classtype: payload.classtype ? String(payload.classtype) : `live`,
                 org_id: payload.org_id ? String(payload.org_id) : ``,
-                isTeacher: payload.teacher ? Boolean(payload.teacher) : null,
+                isTeacher: payload.teacher ? Boolean(payload.teacher) : false,
                 name: payload.name ? String(payload.name) : undefined,
                 roomId: String(payload.roomid),
-                materials: [
-                    {
-                        __typename: MaterialTypename.Iframe,
-                        name: `Local PDF`,
-                        url: `http://localhost:3000/api/pdf/a.pdf`,
-                    },
-                    {
-                        __typename: MaterialTypename.Iframe,
-                        name: `PDF from alpha`,
-                        url: `https://live.alpha.kidsloop.net/assets/60a3c4a203b03c3acdb56f9d.pdf`,
-                    },
-                    {
-                        __typename: MaterialTypename.Iframe,
-                        name: `Colum Presentation`,
-                        url: `/h5p/play/608b9065e5085e0014359f72`,
-                    },
-                ],
+                materials: parsedMaterials || [],
             };
         }
         // TODO think of a better way to set up the debug environment
