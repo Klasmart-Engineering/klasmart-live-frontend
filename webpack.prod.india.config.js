a33b684f0918f65ef11d98e18aad369d6d8396c5
Lee Jelley <lee.jelley@calmid.com>
2021-05-28 10:50:40 +0100

    Fix India deployment scripts

--- webpack.prod.india.config.js
+++ webpack.prod.india.config.js
@@ -84,8 +84,8 @@ module.exports = {
   plugins: [
     new webpack.EnvironmentPlugin(
       { "CALLSTATS_ENABLE": "TRUE" },
-      { "ENDPOINT_CMS": "https://cms.kidsloop.id" },
-      { "ENDPOINT_HUB": "https://hub.kidsloop.id" },
+      { "ENDPOINT_CMS": "https://cms.kidsloop.in" },
+      { "ENDPOINT_HUB": "https://hub.kidsloop.in" },
     ),
     new HtmlWebpackPlugin({
       filename: 'index.html',
