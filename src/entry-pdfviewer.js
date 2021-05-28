0a73b25a67e698fa6eccecf0c352bed14a706361
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-05-28 13:40:03 +0900

    Updates pdf integration ini newuiupdate and altered the initial api for communicating pdf information to pass the data as query aprameters to the iframe instance

--- src/entry-pdfviewer.js
+++ src/entry-pdfviewer.js
@@ -1,94 +1,88 @@
+
 "use strict";
 
-console.log('loading PDF view');
-window.addEventListener('message', (payload) => {
-  const {data} = payload
-  console.log(`message received`);
-  console.log(payload);
-  if(!data.pdfSrc) {
-    return;
-  }
-  if (!pdfjsLib.getDocument || !pdfjsViewer.PDFViewer) {
-    // eslint-disable-next-line no-alert
-    alert("Please build the pdfjs-dist library using\n  `gulp dist-install`");
-  }
-  // The workerSrc property shall be specified.
-  
-  // pdfjsLib.GlobalWorkerOptions.workerSrc =
-  //   "../../node_modules/pdfjs-dist/build/pdf.worker.js";
-  
-  // Some PDFs need external cmaps.
-  
-  const CMAP_URL = "../../node_modules/pdfjs-dist/cmaps/";
-  const CMAP_PACKED = true;
-  
-  const DEFAULT_URL = data.pdfSrc;
-  
-  const container = document.getElementById("viewerContainer");
-  
-  const eventBus = new pdfjsViewer.EventBus();
-  
-  // (Optionally) enable hyperlinks within PDF files.
-  const pdfLinkService = new pdfjsViewer.PDFLinkService({
-    eventBus,
-  });
+const scaleLiterals = [
+    `page-width`,
+    `actual-size`,
+    `automatic-zoom`,
+    `page-fit`,
+];
+let documentSet = false;
+console.log(`loading PDF view`);
+let currentPdfAddress = undefined;
 
-  const pdfViewer = new pdfjsViewer.PDFViewer({
+const params = new URLSearchParams(window.location.search);
+let scale = params.get(`scale`);
+const pdfSrc = params.get(`pdfSrc`);
+
+console.log(params);
+
+if (!pdfjsLib.getDocument || !pdfjsViewer.PDFViewer) {
+// eslint-disable-next-line no-alert
+    alert(`Please build the pdfjs-dist library using\n  \`gulp dist-install\``);
+}
+// The workerSrc property shall be specified.
+
+// pdfjsLib.GlobalWorkerOptions.workerSrc =
+//   "../../node_modules/pdfjs-dist/build/pdf.worker.js";
+
+// Some PDFs need external cmaps.
+
+const CMAP_URL = `../../node_modules/pdfjs-dist/cmaps/`;
+const CMAP_PACKED = true;
+
+const DEFAULT_URL = pdfSrc;
+
+const container = document.getElementById(`viewerContainer`);
+
+const eventBus = new pdfjsViewer.EventBus();
+
+// (Optionally) enable hyperlinks within PDF files.
+const pdfLinkService = new pdfjsViewer.PDFLinkService({
+    eventBus,
+});
+
+const pdfViewer = new pdfjsViewer.PDFViewer({
     container,
     eventBus,
     linkService: pdfLinkService,
-    renderer: "svg",
+    renderer: `svg`,
     textLayerMode: 0,
-    embedFonts: true
-  });
-  pdfLinkService.setViewer(pdfViewer);
+    embedFonts: true,
+});
+pdfLinkService.setViewer(pdfViewer);
 
-  if (!data.scale) data.scale = scaleLiterals[0]
+if (!scale) scale = scaleLiterals[0];
 
-  // validate scale
-  if (typeof data.scale !== 'number' && !scaleLiterals.includes(data.scale)) {
-    console.warn(`Unknown scale literal: ${data.scale}. Defaulting to ${scaleLiterals[0]}`)
-    data.scale = scaleLiterals[0];
-  }
+// validate scale
+if (typeof scale !== `number` && !scaleLiterals.includes(scale)) {
+    console.warn(`Unknown scale literal: ${scale}. Defaulting to ${scaleLiterals[0]}`);
+    scale = scaleLiterals[0];
+}
+console.log('RUNNING A')
+eventBus.on(`pagesinit`, function () {
+// We can use pdfViewer now, e.g. let's change default scale.
+  console.log('RUNNING B')
+    
+  pdfViewer.currentScaleValue = scale;
 
-  eventBus.on("pagesinit", function () {
-    // We can use pdfViewer now, e.g. let's change default scale.
-    pdfViewer.currentScaleValue = data.scale;
-  });
-  
-  // Loading document.
-  const loadingTask = pdfjsLib.getDocument({
+});
+
+// Loading document.
+const loadingTask = pdfjsLib.getDocument({
     url: DEFAULT_URL,
     cMapUrl: CMAP_URL,
     cMapPacked: CMAP_PACKED,
     fontExtraProperties: true,
-  });
-
-  loadingTask.promise.then(function (pdfDocument) {
-    // Document loaded, specifying document for the viewer and
-    // the (optional) linkService.
-    pdfViewer.setDocument(pdfDocument);
-  
-    pdfLinkService.setDocument(pdfDocument, null);
-  });
 });
 
-const scaleLiterals = [
-  'page-width',
-  'actual-size',
-  'automatic-zoom',
-  'page-fit'
-];
+loadingTask.promise.then(function (pdfDocument) {
+// Document loaded, specifying document for the viewer and
+// the (optional) linkService.
 
-// Testing Configuration - Forces display of content from a test PDF server
-// const pdfTest = false;
-// const samplePdfs = ['a', 'b', 'c', 'd'];
-// const selection = samplePdfs[Math.floor(Math.random() * 4)];
-// const data = {
-//   pdfSrc: `http://localhost:3000/api/pdf/${selection}.pdf`,
-//   scale: 'page-width'
-// }
+    pdfViewer.setDocument(pdfDocument);
+    console.log('RUNNING C')
+    
+    pdfLinkService.setDocument(pdfDocument, null);
+});
 
-// if (pdfTest) {
-//   window.postMessage(data, '*');
-// } 
