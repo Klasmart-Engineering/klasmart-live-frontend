13e6743fdd4cbf648cc57e2edc945ac5df9a04fa
Gael <gael.helle@calmid.com>
2021-06-01 08:46:11 +0900

    Feedback translation mistake

--- src/localization/vi.ts
+++ src/localization/vi.ts
@@ -209,7 +209,7 @@ const messages: Record<string, string> = {
     no_students_connected: `Không có học sinh kết nối`,
 
     feedback_detail_question: `Do you want to add more details?`,
-    feedback_comment: `Leave a commnet`,
+    feedback_comment: `Leave a comment`,
 
     feedback_end_class_student_video_1: `Terrible video`,
     feedback_end_class_student_audio_1: `Terrible audio`,
