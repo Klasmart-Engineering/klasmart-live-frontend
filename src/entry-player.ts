a2182eedd54d6025a65d314799b92b0b08f3a8da
Dauren Serkeshev <dauren.serkeshev@calmid.com>
2021-06-04 17:03:07 +0900

    refactor player

--- src/entry-player.ts
+++ src/entry-player.ts
@@ -1,32 +1,27 @@
 import { Replayer } from 'rrweb';
 // eslint-disable-next-line no-unused-vars
-import { eventWithTime } from 'rrweb/typings/types';
 
-let player: Replayer;
-const events: eventWithTime[] = [];
+let hasStarted = false;
+const player: Replayer = new Replayer([], {
+    mouseTail: false,
+    liveMode: true,
+    speed: 1.5,
+});
+player.on(`resize`, () => window.parent.postMessage({
+    width: player.iframe.width,
+    height: player.iframe.height,
+}, `*`));
 
 window.addEventListener(`message`, ({ data }) => {
     if (!data || !data.event) { return; }
     try {
         const event = JSON.parse(data.event);
         console.log(event);
-        if (player) {
-            player.addEvent(event);
-        } else {
-            events.push(event);
-            if (events.length >= 2) {
-                player = new Replayer(events, {
-                    mouseTail: false,
-                    liveMode: true,
-                    speed: 1.5,
-                });
-                player.on(`resize`, () => window.parent.postMessage({
-                    width: player.iframe.width,
-                    height: player.iframe.height,
-                }, `*`));
-                player.startLive(event.timestamp);
-            }
+        if (!hasStarted) {
+            player.startLive(event.timestamp);
+            hasStarted = true;
         }
+        player.addEvent(event);
     } catch (e) {
         console.error(e);
     }
