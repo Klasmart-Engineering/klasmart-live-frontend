a3a9987d30224b49c0b5bf8ac6672b2fad08f9f9
Dauren Serkeshev <dauren.serkeshev@calmid.com>
2021-06-04 16:56:51 +0900

    change dependencies

--- src/entry-player.ts
+++ src/entry-player.ts
@@ -1,26 +1,35 @@
-import { Replayer } from 'rrweb'
+import { Replayer } from 'rrweb';
 // eslint-disable-next-line no-unused-vars
-import { eventWithTime } from 'rrweb/typings/types'
-let player: Replayer
-const events: eventWithTime[] = []
+import { eventWithTime } from 'rrweb/typings/types';
 
-window.addEventListener('message', ({ data }) => {
-  if (!data || !data.event) { return }
-  try {
-    const event = JSON.parse(data.event)
-    if (player) {
-      player.addEvent(event)
-    } else {
-      events.push(event)
-      if (events.length >= 2) {
-        player = new Replayer(events, { mouseTail: false, liveMode: true, speed: 1.5 })
-        player.on('resize', () => window.parent.postMessage({ width: player.iframe.width, height: player.iframe.height }, '*'))
-        player.startLive(event.timestamp)
-      }
+let player: Replayer;
+const events: eventWithTime[] = [];
+
+window.addEventListener(`message`, ({ data }) => {
+    if (!data || !data.event) { return; }
+    try {
+        const event = JSON.parse(data.event);
+        console.log(event);
+        if (player) {
+            player.addEvent(event);
+        } else {
+            events.push(event);
+            if (events.length >= 2) {
+                player = new Replayer(events, {
+                    mouseTail: false,
+                    liveMode: true,
+                    speed: 1.5,
+                });
+                player.on(`resize`, () => window.parent.postMessage({
+                    width: player.iframe.width,
+                    height: player.iframe.height,
+                }, `*`));
+                player.startLive(event.timestamp);
+            }
+        }
+    } catch (e) {
+        console.error(e);
     }
-  } catch (e) {
-    console.error(e)
-  }
 });
-(window as any).PLAYER_READY = true
-window.postMessage('ready','*')
+(window as any).PLAYER_READY = true;
+window.postMessage(`ready`, `*`);
