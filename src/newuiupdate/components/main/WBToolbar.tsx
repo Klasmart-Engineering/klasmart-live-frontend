3028cfb708f1e010aa1bf82b7408a45357586be7
dat <dat.doan@calmid.com>
2021-06-16 10:46:08 +0700

    Update canvas toolbar for CLASS & STUDY

--- src/newuiupdate/components/main/WBToolbar.tsx
+++ src/newuiupdate/components/main/WBToolbar.tsx
@@ -1,27 +1,38 @@
 import StyledIcon from "../../../components/styled/icon";
 import { ClassType } from "../../../store/actions";
-import { WB_EXPAND_BUTTON, WB_TOOLBAR } from "../../../utils/layerValues";
 import { LocalSessionContext } from "../../providers/providers";
-import WBToolbar from "../../whiteboard/components/Toolbar-new"; // TODO: Change Toolbar-new to Toolbar
 import { useSynchronizedState } from "../../whiteboard/context-providers/SynchronizedStateProvider";
 import Fab from "@material-ui/core/Fab";
-import Grid from "@material-ui/core/Grid";
-import IconButton from "@material-ui/core/IconButton";
-import Paper from "@material-ui/core/Paper";
-import { useTheme } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { PencilAlt as WBIcon } from "@styled-icons/fa-solid/PencilAlt";
-import { Close as CloseIcon } from "@styled-icons/material/Close";
-import { ExpandLess as ArrowUpIcon } from "@styled-icons/material/ExpandLess";
-import { ExpandMore as ArrowDownIcon } from "@styled-icons/material/ExpandMore";
-import React, { useContext, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
+import CanvasMenu from "../toolbar/toolbarMenus/canvasMenu";
+import {useRecoilState} from "recoil";
+import {isCanvasOpenState} from "../../states/layoutAtoms";
+import {
+    Grid,
+    makeStyles,
+} from "@material-ui/core";
 
 export const WB_TOOLBAR_MAX_HEIGHT = 80; // 64 + 16(padding top)
-export const MOBILE_WB_TOOLBAR_MAX_HEIGHT = 46; // 38 + 8(padding top)
+
+const useStyles = makeStyles(() => ({
+    root: {
+        position: `relative`,
+        height: WB_TOOLBAR_MAX_HEIGHT
+    },
+    buttonWrap: {
+        position: `absolute`,
+        zIndex: 999,
+        bottom: 10,
+        left: 10,
+        paddingTop: `15px`
+    },
+}));
 
 export function WBToolbarContainer({ useLocalDisplay } : { useLocalDisplay?: boolean} ) {
-    const theme = useTheme();
-    const isSmDown = useMediaQuery(theme.breakpoints.down(`sm`));
+    const classes = useStyles();
+    const canvasRef = React.useRef<any>();
+    const [ isCanvasOpen, setIsCanvasOpen ] = useRecoilState(isCanvasOpenState);
 
     const {
         classtype, isTeacher, sessionId,
@@ -31,8 +42,8 @@ export function WBToolbarContainer({ useLocalDisplay } : { useLocalDisplay?: boo
             setDisplay, setLocalDisplay, getPermissions, setPermissions,
         },
     } = useSynchronizedState();
+
     const enableWB = classtype === ClassType.LIVE ? (!isTeacher ? display && permissions.allowCreateShapes : display) : true;
-    const [ open, setOpen ] = useState(false);
 
     const handleOpenWBToolbar = (e: React.MouseEvent<HTMLButtonElement>) => {
         if (useLocalDisplay) {
@@ -41,9 +52,9 @@ export function WBToolbarContainer({ useLocalDisplay } : { useLocalDisplay?: boo
             setDisplay(true);
         }
 
-        setOpen(true);
         if (classtype !== ClassType.LIVE) {
-            setDisplay(true);
+            setIsCanvasOpen(!isCanvasOpen);
+            setDisplay(!display);
             const permissions = getPermissions(sessionId);
             const newPermissions = {
                 ...permissions,
@@ -59,7 +70,6 @@ export function WBToolbarContainer({ useLocalDisplay } : { useLocalDisplay?: boo
             setDisplay(false);
         }
         
-        setOpen(false);
         if (classtype !== ClassType.LIVE) {
             setDisplay(false);
             const permissions = getPermissions(sessionId);
@@ -70,72 +80,33 @@ export function WBToolbarContainer({ useLocalDisplay } : { useLocalDisplay?: boo
             setPermissions(sessionId, newPermissions);
         }
     };
+
+    useEffect(() => {
+        display && setIsCanvasOpen(permissions.allowCreateShapes);
+        !display && setIsCanvasOpen(false);
+    }, [ display, permissions.allowCreateShapes ]);
+
     return (
         <Grid
             item
             xs={12}
-            style={{
-                position: `relative`,
-                height: WB_TOOLBAR_MAX_HEIGHT,
-            }}>
-            <Fab
-                aria-label="whiteboard toolbar opener"
-                disabled={!enableWB}
-                size="large"
-                style={{
-                    display: open ? `none` : `flex`,
-                    backgroundColor:`#bd6dd6`,
-                    zIndex: WB_EXPAND_BUTTON,
-                    position: `absolute`,
-                    bottom: 10,
-                    left: 10,
-                }}
-                onClick={handleOpenWBToolbar}
-            >
-                <StyledIcon
-                    icon={<WBIcon />}
+            className={classes.root}>
+            <div ref={canvasRef}
+                 className={classes.buttonWrap}>
+                <Fab
+                    aria-label="whiteboard toolbar opener"
+                    disabled={!enableWB}
                     size="large"
-                    color="white" />
-            </Fab>
-            <Paper
-                aria-label="whiteboard toolbar"
-                elevation={2}
-                style={{
-                    zIndex: WB_TOOLBAR,
-                    display: open ? `flex` : `none`,
-                    padding: isSmDown ? theme.spacing(0.5) : theme.spacing(1),
-                    width: `calc(100% - 20px)`,
-                    position: `absolute`,
-                    bottom: 5,
-                    borderRadius: 32,
-                    overflowY: `hidden`,
-                }}
-            >
-                <Grid
-                    container
-                    direction="row"
-                    justify="space-between"
-                    alignItems="center">
-                    <Grid
-                        item
-                        style={{
-                            flex: 0,
-                        }}>
-                        <IconButton
-                            size={`medium`}
-                            style={{
-                                backgroundColor: theme.palette.background.paper,
-                            }}
-                            onClick={handleCloseWBToolbar}
-                        >
-                            <StyledIcon
-                                icon={isTeacher ? <ArrowDownIcon /> : <CloseIcon />}
-                                size="large" />
-                        </IconButton>
-                    </Grid>
-                    <WBToolbar />
-                </Grid>
-            </Paper>
+                    color="primary"
+                    onClick={handleOpenWBToolbar}
+                >
+                    <StyledIcon
+                        icon={<WBIcon />}
+                        size="large"
+                        color="white" />
+                </Fab>
+            </div>
+            <CanvasMenu anchor={canvasRef.current} />
         </Grid>
     );
 }
