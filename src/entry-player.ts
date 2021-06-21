d9e525f6a50c22b05ec09a50c00d9d156b95397f
Dauren Serkeshev <dauren.serkeshev@calmid.com>
2021-06-21 19:43:06 +0900

    working oon basic level

--- src/entry-player.ts
+++ src/entry-player.ts
@@ -10,10 +10,12 @@ const player: Replayer = new Replayer([], {
     UNSAFE_replayCanvas: true,
 });
 player.enableInteract();
+
 player.on(`resize`, () => window.parent.postMessage({
     width: player.iframe.width,
     height: player.iframe.height,
 }, `*`));
+
 player.on(`custom-event`, (event: any) => {
     console.log(`received custom event`, event);
     if(!event || !event.data){
@@ -21,9 +23,10 @@ player.on(`custom-event`, (event: any) => {
     }
     if(event.data.tag === `stateChange`) {
         ytPlayer.playVideo();
-
     }
 });
+
+player.on(`fullsnapshot-rebuilded`, (event: any) => onYTAPIReady());
 
 window.addEventListener(`message`, ({ data }) => {
     if (!data || !data.event) { return; }
@@ -42,18 +45,44 @@ window.addEventListener(`message`, ({ data }) => {
 (window as any).PLAYER_READY = true;
 window.postMessage(`ready`, `*`);
 
-if (!(window as any).YT) {
-    const tag = document.createElement(`script`);
-    tag.src = `https://www.youtube.com/iframe_api`;
-    (window as any).onYouTubeIframeAPIReady = onYTAPIReady;
-    const firstScriptTag = document.getElementsByTagName(`script`)[0];
-    firstScriptTag?.parentNode?.insertBefore(tag, firstScriptTag);
-    console.log(`replayed page loaded YouTube iframe api`);
-} else {
-    onYTAPIReady();
-}
-
 function onYTAPIReady () {
-    ytPlayer = new (window as any).YT.Player(`h5p-youtube-0`, {});
-    console.log(`replayed page mounted YouTube player object`, ytPlayer);
+    const replayedIframe = window.document.getElementsByTagName(`iframe`)[0];
+    const replayedWindow = replayedIframe.contentWindow;
+
+    const dummyTag = replayedWindow?.document.createElement(`script`);
+    const tag = replayedWindow?.document.createElement(`script`);
+
+    if (!tag || !dummyTag) {
+        return;
+    }
+    (replayedWindow as any).onYouTubeIframeAPIReady = callback;
+    console.log(`document`, replayedWindow?.document);
+    const body = replayedWindow?.document.getElementsByTagName(`body`)[0];
+    console.log(`body`, body);
+    const head = replayedWindow?.document.getElementsByTagName(`head`)[0];
+    head?.appendChild(dummyTag);
+    tag.src = `https://www.youtube.com/iframe_api`;
+    console.log(`head`, head, `tag`, tag);
+    head?.appendChild(tag);
+
+    function callback () {
+        const doc = replayedIframe.contentDocument;
+        if (!doc) {
+            console.log(`empty replayed doc`);
+            return;
+        }
+        console.log(`doc`, doc.getElementsByTagName(`iframe`));
+        for(const iframe of doc.getElementsByTagName(`iframe`)) {
+            const src = (iframe as HTMLIFrameElement).getAttribute(`src`) ?? ``;
+            const url = new URL(src);
+            if (url.origin === `https://www.youtube.com`) {
+                const updatedSrc = url.origin +  url.pathname + `?enablejsapi=1`;
+                (iframe as HTMLIFrameElement).setAttribute(`src`, updatedSrc);
+            }
+            const id = (iframe as HTMLIFrameElement).getAttribute(`id`);
+            console.log(`iframe`, iframe, `id`, id);
+            ytPlayer = new (replayedWindow as any).YT.Player(id, {});
+        }
+        console.log(`replayed page mounted YouTube  object`, ytPlayer);
+    }
 }
