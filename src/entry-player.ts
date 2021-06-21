ddbaa458345770b2db714007e3d6200c07311fda
Dauren Serkeshev <dauren.serkeshev@calmid.com>
2021-06-21 17:03:10 +0900

    add youtube iframe api

--- src/entry-player.ts
+++ src/entry-player.ts
@@ -1,6 +1,7 @@
 import { Replayer } from 'rrweb';
 // eslint-disable-next-line no-unused-vars
 
+let ytPlayer: any;
 let hasStarted = false;
 const player: Replayer = new Replayer([], {
     mouseTail: false,
@@ -8,10 +9,21 @@ const player: Replayer = new Replayer([], {
     speed: 1.5,
     UNSAFE_replayCanvas: true,
 });
+player.enableInteract();
 player.on(`resize`, () => window.parent.postMessage({
     width: player.iframe.width,
     height: player.iframe.height,
 }, `*`));
+player.on(`custom-event`, (event: any) => {
+    console.log(`received custom event`, event);
+    if(!event || !event.data){
+        return;
+    }
+    if(event.data.tag === `stateChange`) {
+        ytPlayer.playVideo();
+
+    }
+});
 
 window.addEventListener(`message`, ({ data }) => {
     if (!data || !data.event) { return; }
@@ -29,3 +41,19 @@ window.addEventListener(`message`, ({ data }) => {
 });
 (window as any).PLAYER_READY = true;
 window.postMessage(`ready`, `*`);
+
+if (!(window as any).YT) {
+    const tag = document.createElement(`script`);
+    tag.src = `https://www.youtube.com/iframe_api`;
+    (window as any).onYouTubeIframeAPIReady = onYTAPIReady;
+    const firstScriptTag = document.getElementsByTagName(`script`)[0];
+    firstScriptTag?.parentNode?.insertBefore(tag, firstScriptTag);
+    console.log(`replayed page loaded YouTube iframe api`);
+} else {
+    onYTAPIReady();
+}
+
+function onYTAPIReady () {
+    ytPlayer = new (window as any).YT.Player(`h5p-youtube-0`, {});
+    console.log(`replayed page mounted YouTube player object`, ytPlayer);
+}
--- src/entry-record.ts
+++ src/entry-record.ts
@@ -1,60 +1,93 @@
-import { record } from 'rrweb'
-import { GraphQLClient } from 'graphql-request'
-import { EventRecorderService } from './services/event-recorder/EventRecorderService'
-import { GraphQlUploader } from './services/event-recorder/uploader/GraphQlUploader'
-import { EventStream } from './services/event-recorder/stream/EventStream'
-import { AuthTokenProvider } from './services/auth-token/AuthTokenProvider'
-import { v4 as uuid } from 'uuid'
+import { AuthTokenProvider } from './services/auth-token/AuthTokenProvider';
+import { EventRecorderService } from './services/event-recorder/EventRecorderService';
+import { EventStream } from './services/event-recorder/stream/EventStream';
+import { GraphQlUploader } from './services/event-recorder/uploader/GraphQlUploader';
+import { GraphQLClient } from 'graphql-request';
+import { record } from 'rrweb';
+import { v4 as uuid } from 'uuid';
+
 if (!(window as any).kidslooplive) {
-  (window as any).kidslooplive = true
+    (window as any).kidslooplive = true;
 
-
-
-  const POST_URL = `${process.env.ENDPOINT_GQL || window.location.origin}/graphql`
-  const POST_EVENTS = `
+    const POST_URL = `${process.env.ENDPOINT_GQL || window.location.origin}/graphql`;
+    const POST_EVENTS = `
   mutation postPageEvent($streamId: ID!, $pageEvents: [PageEventIn]) {
     postPageEvent(streamId: $streamId, pageEvents: $pageEvents)
   }
-  `
+  `;
 
-  const token = AuthTokenProvider.retrieveToken()
+    const token = AuthTokenProvider.retrieveToken();
 
-  const headers = token ? { authorization: `Bearer ${token}` } : undefined
+    const headers = token ? {
+        authorization: `Bearer ${token}`,
+    } : undefined;
 
-  const client = new GraphQLClient(POST_URL, { headers: headers })
+    const client = new GraphQLClient(POST_URL, {
+        headers: headers,
+    });
 
-  const streamId = uuid()
-  window.parent.postMessage({ streamId }, '*')
+    const streamId = uuid();
+    window.parent.postMessage({
+        streamId,
+    }, `*`);
 
-  const eventStream = EventStream.builder()
-    .withId(streamId)
-    .build()
+    const eventStream = EventStream.builder()
+        .withId(streamId)
+        .build();
 
-  const uploader = new GraphQlUploader(client, POST_EVENTS)
+    const uploader = new GraphQlUploader(client, POST_EVENTS);
 
-  const eventRecorder = EventRecorderService.builder()
-    .withUploader(uploader)
-    .withUploadRetryTimeoutMillis(500)
-    .build()
+    const eventRecorder = EventRecorderService.builder()
+        .withUploader(uploader)
+        .withUploadRetryTimeoutMillis(500)
+        .build();
 
-  let eventsSinceKeyframe = 0
+    let eventsSinceKeyframe = 0;
 
-  record({
-    checkoutEveryNms: 1000 * 60,
-    checkoutEveryNth: 512,
-    emit: (e, c) => {
-      // TODO: Should client or server keep track of the
-      // number of events emitted since last keyframe?
-      if (c) { eventsSinceKeyframe = 0 }
+    record({
+        checkoutEveryNms: 1000 * 60,
+        emit: (e, c) => {
+            // TODO: Should client or server keep track of the
+            // number of events emitted since last keyframe?
+            if (c) { eventsSinceKeyframe = 0; }
 
-      const eventData = JSON.stringify({
-        checkout: c || false,
-        event: e,
-        index: eventsSinceKeyframe++
-      })
+            const eventData = JSON.stringify({
+                checkout: c || false,
+                event: e,
+                index: eventsSinceKeyframe++,
+            });
 
-      eventRecorder.recordEvent(eventStream, eventData, c || false)
-      eventRecorder.uploadEvents()
-    }
-  })
+            eventRecorder.recordEvent(eventStream, eventData, c || false);
+            eventRecorder.uploadEvents();
+        },
+    });
+}
+
+if (!(window as any).YT) {
+    const tag = document.createElement(`script`);
+    tag.src = `https://www.youtube.com/iframe_api`;
+    (window as any).onYouTubeIframeAPIReady = onYTAPIReady;
+    const firstScriptTag = document.getElementsByTagName(`script`)[0];
+    firstScriptTag?.parentNode?.insertBefore(tag, firstScriptTag);
+    console.log(`recorded page loaded YouTube iframe api`);
+} else {
+    onYTAPIReady();
+}
+
+function onYTAPIReady () {
+    const onPlayerReady = (event: any) => {
+        console.log(`onPlayerReady`, event);
+        ytPlayer.playVideo();
+    };
+    const onPlayerStateChange = (event: any) => {
+        console.log(`onPlayerStateChange`, event);
+        record.addCustomEvent(`stateChange`, event);
+    };
+    const ytPlayer = new (window as any).YT.Player(`h5p-yoiutube-0`, {
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange,
+        },
+    });
+    console.log(`recorded page mounted YouTube player object`, ytPlayer);
 }
