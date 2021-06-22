c5ba9ab6a39045f1555a16f3408e24eee917ca5f
Dauren Serkeshev <dauren.serkeshev@calmid.com>
2021-06-22 17:58:58 +0900

    support youtube url in h5p

--- src/entry-player.ts
+++ src/entry-player.ts
@@ -1,38 +1,110 @@
-import { Replayer } from 'rrweb'
+import { Replayer } from 'rrweb';
 // eslint-disable-next-line no-unused-vars
-import { eventWithTime } from 'rrweb/typings/types'
-let player: Replayer
-const events: eventWithTime[] = []
-let receivedSnapshot = false;
 
-window.addEventListener('message', ({ data }) => {
-  if (!data || !data.event) { return }
-  try {
-    const event = JSON.parse(data.event);
-    const isSnapshot = event.type === 2;
-    receivedSnapshot = receivedSnapshot || isSnapshot;
-    
-    if (player) {
-      if (receivedSnapshot && isSnapshot) {
-        return;
-      }
-      
-      player.addEvent(event)
-    } else {
-      events.push(event)
-      if (events.length >= 2) {
-        player = new Replayer(events, { mouseTail: false, liveMode: true, speed: 1.5 })
-        player.on('resize', () => window.parent.postMessage({ width: player.iframe.width, height: player.iframe.height }, '*'))
-        player.startLive(event.timestamp);
-      }
-    }
-  } catch (e) {
-    console.error(e)
-  }
+enum YoutubePlayerState {
+    ENDED = 0,
+    PLAYING,
+    PAUSED,
+    BUFFERING,
+    CUED,
+}
+const youtubePlayers = new Map<string, any>();
+let hasStarted = false;
+const player: Replayer = new Replayer([], {
+    mouseTail: false,
+    liveMode: true,
+    speed: 1.5,
+    UNSAFE_replayCanvas: true,
 });
-(window as any).PLAYER_READY = true
 
-window.postMessage('ready','*')
+player.on(`resize`, () => window.parent.postMessage({
+    width: player.iframe.width,
+    height: player.iframe.height,
+}, `*`));
 
-// NOTE: Added this to notify the preview-player that the entry is ready to handle received events.
-window.parent.postMessage('ready', '*');
+player.on(`custom-event`, (event: any) => {
+    console.log(`received custom event`, event);
+    if(!event || !event.data){
+        return;
+    }
+    const { tag, payload } = event.data;
+    const youtubePlayer = youtubePlayers.get(payload.id);
+    if (!youtubePlayer) {
+        return;
+    }
+    if(tag === `stateChange`) {
+        const info = payload.playerInfo;
+        youtubePlayer.seekTo(info.currentTime);
+        switch(info.playerState){
+        case YoutubePlayerState.ENDED:
+            youtubePlayer.stopVideo();
+            break;
+        case YoutubePlayerState.PLAYING:
+            youtubePlayer.playVideo();
+            break;
+        case YoutubePlayerState.PAUSED:
+            youtubePlayer.pauseVideo();
+            break;
+        default:
+            break;
+        }
+    }
+});
+
+player.on(`fullsnapshot-rebuilded`, () => onFullSnapshotRebuilded());
+
+window.addEventListener(`message`, ({ data }) => {
+    if (!data || !data.event) { return; }
+    try {
+        const event = JSON.parse(data.event);
+        // console.log(event);
+        if (!hasStarted) {
+            player.startLive(event.timestamp);
+            hasStarted = true;
+        }
+        player.addEvent(event);
+    } catch (e) {
+        console.error(e);
+    }
+});
+(window as any).PLAYER_READY = true;
+window.postMessage(`ready`, `*`);
+
+function onFullSnapshotRebuilded () {
+    console.log(`onFullSnapshotRebuilded`);
+    youtubePlayers.clear();
+    const replayedIframe = window.document.getElementsByTagName(`iframe`)[0];
+    const replayedWindow = replayedIframe.contentWindow;
+
+    const onYTAPIReady = () => {
+        if (!replayedWindow?.document) {
+            return;
+        }
+        for(const iframe of replayedWindow?.document.getElementsByTagName(`iframe`)) {
+            const src = (iframe as HTMLIFrameElement).getAttribute(`src`) ?? ``;
+            const url = new URL(src);
+            if (url.origin !== `https://www.youtube.com`) {
+                continue;
+            }
+            const updatedSrc = url.origin +  url.pathname + `?enablejsapi=1`;
+            (iframe as HTMLIFrameElement).setAttribute(`src`, updatedSrc);
+            const id = (iframe as HTMLIFrameElement).getAttribute(`id`) ?? ``;
+            const youtubePlayer = new (replayedWindow as any).YT.Player(id, {});
+            youtubePlayers.set(id, youtubePlayer);
+            console.log(`replayed page got reference to YT player`, youtubePlayer, `id`, id);
+        }
+    };
+
+    if (!(replayedWindow as any).YT) {
+        const tag = replayedWindow?.document.createElement(`script`);
+        (replayedWindow as any).onYouTubeIframeAPIReady = onYTAPIReady;
+        const head = replayedWindow?.document.getElementsByTagName(`head`)[0];
+        if(!tag) {
+            return;
+        }
+        tag.src = `https://www.youtube.com/iframe_api`;
+        head?.appendChild(tag);
+    } else {
+        onYTAPIReady();
+    }
+}
--- src/entry-record.ts
+++ src/entry-record.ts
@@ -1,62 +1,114 @@
-import { record } from 'rrweb'
-import { GraphQLClient } from 'graphql-request'
-import { EventRecorderService } from './services/event-recorder/EventRecorderService'
-import { GraphQlUploader } from './services/event-recorder/uploader/GraphQlUploader'
-import { EventStream } from './services/event-recorder/stream/EventStream'
-import { AuthTokenProvider } from './services/auth-token/AuthTokenProvider'
-import { v4 as uuid } from 'uuid'
-if (!(window as any).kidslooplive) {
-  (window as any).kidslooplive = true
+import { AuthTokenProvider } from './services/auth-token/AuthTokenProvider';
+import { EventRecorderService } from './services/event-recorder/EventRecorderService';
+import { EventStream } from './services/event-recorder/stream/EventStream';
+import { GraphQlUploader } from './services/event-recorder/uploader/GraphQlUploader';
+import { GraphQLClient } from 'graphql-request';
+import { record } from 'rrweb';
+import { v4 as uuid } from 'uuid';
 
-  const POST_EVENTS = `
+if (!(window as any).kidslooplive) {
+    (window as any).kidslooplive = true;
+
+    const POST_URL = `${process.env.ENDPOINT_GQL || window.location.origin}/graphql`;
+    const POST_EVENTS = `
   mutation postPageEvent($streamId: ID!, $pageEvents: [PageEventIn]) {
     postPageEvent(streamId: $streamId, pageEvents: $pageEvents)
   }
-  `
+  `;
 
-  const url = new URL(window.location.href)
-  const endpoint = url.searchParams.get('endpoint') || window.location.origin;
+    const token = AuthTokenProvider.retrieveToken();
 
-  const token = AuthTokenProvider.retrieveToken()
+    const headers = token ? {
+        authorization: `Bearer ${token}`,
+    } : undefined;
 
-  const headers = token ? { authorization: `Bearer ${token}` } : undefined
+    const client = new GraphQLClient(POST_URL, {
+        headers: headers,
+    });
 
-  const graphQlEndpoint = `${decodeURIComponent(endpoint)}/graphql`;
-  const client = new GraphQLClient(graphQlEndpoint, { headers: headers })
+    const streamId = uuid();
+    window.parent.postMessage({
+        streamId,
+    }, `*`);
 
-  const streamId = uuid()
+    const eventStream = EventStream.builder()
+        .withId(streamId)
+        .build();
 
-  window.parent.postMessage({ streamId }, '*')
+    const uploader = new GraphQlUploader(client, POST_EVENTS);
 
-  const eventStream = EventStream.builder()
-    .withId(streamId)
-    .build()
+    const eventRecorder = EventRecorderService.builder()
+        .withUploader(uploader)
+        .withUploadRetryTimeoutMillis(500)
+        .build();
 
-  const uploader = new GraphQlUploader(client, POST_EVENTS)
+    let eventsSinceKeyframe = 0;
 
-  const eventRecorder = EventRecorderService.builder()
-    .withUploader(uploader)
-    .withUploadRetryTimeoutMillis(500)
-    .build()
+    record({
+        checkoutEveryNms: 1000 * 10,
+        emit: (e, c) => {
+            // TODO: Should client or server keep track of the
+            // number of events emitted since last keyframe?
+            if (c) { eventsSinceKeyframe = 0; }
 
-  let eventsSinceKeyframe = 0
+            const eventData = JSON.stringify({
+                checkout: c || false,
+                event: e,
+                index: eventsSinceKeyframe++,
+            });
 
-  record({
-    checkoutEveryNms: 1000 * 60,
-    checkoutEveryNth: 512,
-    emit: (e, c) => {
-      // TODO: Should client or server keep track of the
-      // number of events emitted since last keyframe?
-      if (c) { eventsSinceKeyframe = 0 }
+            eventRecorder.recordEvent(eventStream, eventData, c || false);
+            eventRecorder.uploadEvents();
+        },
+        allowIframe,
+    });
+}
 
-      const eventData = JSON.stringify({
-        checkout: c || false,
-        event: e,
-        index: eventsSinceKeyframe++
-      })
+if (!(window as any).YT) {
+    const tag = document.createElement(`script`);
+    tag.src = `https://www.youtube.com/iframe_api`;
+    (window as any).onYouTubeIframeAPIReady = onYTAPIReady;
+    const firstScriptTag = document.getElementsByTagName(`script`)[0];
+    firstScriptTag?.parentNode?.insertBefore(tag, firstScriptTag);
+    console.log(`recorded page loaded YouTube iframe api`);
+} else {
+    onYTAPIReady();
+}
 
-      eventRecorder.recordEvent(eventStream, eventData, c || false)
-      eventRecorder.uploadEvents()
+function onYTAPIReady () {
+    const onPlayerReady = (id: string) => (event: any) => {
+        console.log(`onPlayerReady`, `id`, id, `event`, event);
+    };
+    const onPlayerStateChange = (id: string) => (event: any) => {
+        console.log(`onPlayerStateChange`, `id`, id, `event`, event);
+        record.addCustomEvent(`stateChange`, {
+            id,
+            playerInfo: event.target.playerInfo,
+        });
+    };
+    for(const iframe of document.getElementsByTagName(`iframe`)) {
+        const src = (iframe as HTMLIFrameElement).getAttribute(`src`) ?? ``;
+        const url = new URL(src);
+        if (url.origin !== `https://www.youtube.com`) {
+            continue;
+        }
+        const id = (iframe as HTMLIFrameElement).getAttribute(`id`) ?? ``;
+        const player = new (window as any).YT.Player(id, {
+            events: {
+                onReady: onPlayerReady(id),
+                onStateChange: onPlayerStateChange(id),
+            },
+        });
+        console.log(`recorded page got reference to YT player`, player, `id`, id);
     }
-  })
+
+}
+
+function allowIframe (src: string): boolean {
+    try {
+        const url = new URL(src);
+        return url.origin === `https://www.youtube.com`;
+    } catch {
+        return false;
+    }
 }
