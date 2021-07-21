6d271383b28782c78924aaf44010d0b9a55b8864
Axel Wettervik <axel.wettervik@gmail.com>
2021-07-21 14:02:34 +0900

    Added functions for selecting a file or image.
    -> Select file from Gallery
    -> Select file from Camera
    -> Select file from device files

--- src/services/files/FileSelectService.ts
+++ src/services/files/FileSelectService.ts
@@ -1,5 +1,9 @@
 import { IFileSelectService } from "./IFileSelectService";
 
+const DESTINATION_FILE_URI = 1;
+const SOURCE_PHOTOLIBRARY = 0;
+const SOURCE_CAMERA = 1;
+
 export class FileSelectService implements IFileSelectService {
     async selectFile(): Promise<File> {
         const input: HTMLInputElement = document.createElement(`input`);
@@ -27,57 +31,43 @@ export class FileSelectService implements IFileSelectService {
         }
     }
 
-    async selectFromGallery(): Promise<File> {
-        const input: HTMLInputElement = document.createElement(`input`);
-        input.type = `file`;
-        input.accept = `image/*`;
+    selectFromGallery(): Promise<string> {
+        const camera = (navigator as any).camera;
+        if (!camera) return Promise.reject(`No camera available.`);
 
-        var selectFileProcedure = new Promise<File>((resolve, reject) => {
-            input.onchange = (e: Event) => {
-                const target = e.target as HTMLInputElement ?? undefined;
-                if (target && target.files?.length) {
-                    resolve(target.files[0]);
-                } else {
-                    reject("No file was selected.");
-                }
-            };
+        const options = {
+            sourceType: SOURCE_PHOTOLIBRARY,
+            destinationType: DESTINATION_FILE_URI
+        };
 
-            input.click();
+        var selectGalleryProcedure = new Promise<string>((resolve, reject) => {
+            camera.getPicture((uri: string) => {
+                resolve(uri);
+            }, (message: string) => {
+                reject(message);
+            }, options)
         });
 
-        try {
-            return await selectFileProcedure;
-        } catch (error) {
-            return Promise.reject(error);
-        } finally {
-            input.remove();
-        }
+        return selectGalleryProcedure;
     }
 
-    async selectFromCamera(): Promise<File> {
-        const input: HTMLInputElement = document.createElement(`input`);
-        input.type = `file`;
-        input.accept = `image/*;capture=camera`;
+    async selectFromCamera(): Promise<string> {
+        const camera = (navigator as any).camera;
+        if (!camera) return Promise.reject(`No camera available.`);
 
-        var selectFileProcedure = new Promise<File>((resolve, reject) => {
-            input.onchange = (e: Event) => {
-                const target = e.target as HTMLInputElement ?? undefined;
-                if (target && target.files?.length) {
-                    resolve(target.files[0]);
-                } else {
-                    reject("No file was selected.");
-                }
-            };
+        const options = {
+            sourceType: SOURCE_CAMERA,
+            destinationType: DESTINATION_FILE_URI
+        };
 
-            input.click();
+        var selectGalleryProcedure = new Promise<string>((resolve, reject) => {
+            camera.getPicture((uri: string) => {
+                resolve(uri);
+            }, (message: string) => {
+                reject(message);
+            }, options)
         });
 
-        try {
-            return await selectFileProcedure;
-        } catch (error) {
-            return Promise.reject(error);
-        } finally {
-            input.remove();
-        }
+        return selectGalleryProcedure;
     }
 }
