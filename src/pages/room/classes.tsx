494748026e2fde848ed1fb90a4f3b3de17650221
Owen Delahoy <owen.delahoy@gmail.com>
2021-07-21 14:25:13 +0900

    Remove old/deprecated RoomProvider

--- src/pages/room/classes.tsx
+++ src/pages/room/classes.tsx
@@ -1,40 +1,44 @@
-import React, { useContext, useState } from "react";
-import { useSelector } from "react-redux";
 import { LocalSessionContext } from "../../entry";
-import { RoomProvider } from "../../providers/RoomContext";
 import { State } from "../../store/store";
 import { GlobalWhiteboardContext } from "../../whiteboard/context-providers/GlobalWhiteboardContext";
 import { Layout } from "./layout-new";
-import { InteractiveModeState, StreamIdState } from "./room";
+import {
+    InteractiveModeState,
+    StreamIdState,
+} from "./room";
+import React,
+{
+    useContext,
+    useState,
+} from "react";
+import { useSelector } from "react-redux";
 
 interface ClassesProps {
     interactiveModeState: InteractiveModeState;
     streamIdState: StreamIdState;
 }
 
-export function Classes({
+export function Classes ({
     interactiveModeState,
     streamIdState,
 }: ClassesProps): JSX.Element {
     const { materials } = useContext(LocalSessionContext);
     const contentIndex = useSelector((store: State) => store.control.contentIndex);
     const material = contentIndex >= 0 && contentIndex < materials.length ? materials[contentIndex] : undefined;
-    const [tabIndex, setTabIndex] = useState(0);
-    const [materialKey, setMaterialKey] = useState(Math.random());
+    const [ tabIndex, setTabIndex ] = useState(0);
+    const [ materialKey, setMaterialKey ] = useState(Math.random());
 
     return (
-        <RoomProvider>
-            <GlobalWhiteboardContext>
-                <Layout
-                    interactiveModeState={interactiveModeState}
-                    streamIdState={streamIdState}
-                    material={material}
-                    materialKey={materialKey}
-                    setMaterialKey={setMaterialKey}
-                    tabIndex={tabIndex}
-                    setTabIndex={setTabIndex}
-                />
-            </GlobalWhiteboardContext>
-        </RoomProvider>
+        <GlobalWhiteboardContext>
+            <Layout
+                interactiveModeState={interactiveModeState}
+                streamIdState={streamIdState}
+                material={material}
+                materialKey={materialKey}
+                setMaterialKey={setMaterialKey}
+                tabIndex={tabIndex}
+                setTabIndex={setTabIndex}
+            />
+        </GlobalWhiteboardContext>
     );
 }
