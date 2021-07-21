29aff67b728f3546443d893104ec4066ccd1ff99
Axel Wettervik <axel.wettervik@gmail.com>
2021-07-21 14:10:05 +0900

    Add a TODO comment about file service result types.

--- src/services/files/IFileSelectService.ts
+++ src/services/files/IFileSelectService.ts
@@ -1,5 +1,7 @@
 
 export interface IFileSelectService {
+    // TODO: Change the `File` or `string` to be uniform across all the functions. So that UI using
+    // this service can handle all of them the same way.
     selectFile(): Promise<File>;
     selectFromGallery(): Promise<string>;
     selectFromCamera(): Promise<string>;
