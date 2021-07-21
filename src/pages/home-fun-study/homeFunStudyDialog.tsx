608660edd5d6556ec71d13eabda409912f84bc97
Hung Huynh <hung.huynh@calmid.com>
2021-07-21 22:22:07 +0700

    Added local saved for comments

--- src/pages/home-fun-study/homeFunStudyDialog.tsx
+++ src/pages/home-fun-study/homeFunStudyDialog.tsx
@@ -333,7 +333,6 @@ function HomeFunStudyAssignment({
 
 function HomeFunStudyComment({studyId, defaultComment}: { studyId?: string, defaultComment: string}) {
     const classes = useStyles();
-
     const dispatch = useDispatch();
     const [openEditComment, setOpenEditComment] = useState(false);
     const [comment, setComment] = useState(defaultComment);
@@ -372,6 +371,7 @@ function HomeFunStudyComment({studyId, defaultComment}: { studyId?: string, defa
         }
         setOpenEditComment(false);
     }
+    
     function handleCloseComment(){
         if(studyId){
             let newHFSComments = homeFunStudyComments ? homeFunStudyComments.slice() : [];
