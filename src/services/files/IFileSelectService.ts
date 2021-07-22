05c942ba1408fef670ac5c0c1bd74fb99bd79744
Hung Huynh <hung.huynh@calmid.com>
2021-07-22 20:48:53 +0700

    Added validation of files and cordova-plugin-file

--- src/services/files/IFileSelectService.ts
+++ src/services/files/IFileSelectService.ts
@@ -3,6 +3,6 @@ export interface IFileSelectService {
     // TODO: Change the `File` or `string` to be uniform across all the functions. So that UI using
     // this service can handle all of them the same way.
     selectFile(): Promise<File>;
-    selectFromGallery(): Promise<string>;
-    selectFromCamera(): Promise<string>;
+    selectFromGallery(): Promise<File>;
+    selectFromCamera(): Promise<File>;
 }
