09d4c9b424c36ab2cf849ab4e2696504b994cab7
Hung Huynh <hung.huynh@calmid.com>
2021-07-22 21:53:37 +0700

    Re-handle confirm dialog

---
+++ src/components/dialogs/baseConfirmDialog.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {createStyles, makeStyles} from "@material-ui/core/styles";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        rounded_dialog: {
+            borderRadius: "12px",
+        },
+        dialogTitle: {
+            backgroundColor: "#FFCC00",
+        },
+        dialogText: {
+            color: "#142C45"
+        },
+        buttonClose: {
+            color: "#193756",
+            backgroundColor: "#D6D6D6",
+            borderRadius: 15
+        },
+        buttonConfirm: {
+            color: "white",
+            backgroundColor: "#193756",
+            borderRadius: 15,
+            marginLeft: theme.spacing(1)
+        }
+    })
+)
+
+interface Props {
+    open: boolean,
+    onClose: () => void,
+    onConfirm: () => void,
+    title: string,
+    closeLabel: string,
+    confirmLabel: string,
+    children: React.ReactNode
+}
+
+export function BaseConfirmDialog({
+                                  open, onClose, onConfirm, title,
+                                  closeLabel, confirmLabel, children
+                              }: Props): JSX.Element {
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <Dialog fullWidth maxWidth={'sm'} className={classes.rounded_dialog}
+                    classes={{paper: classes.rounded_dialog}} open={open} onClose={onClose}>
+                <DialogTitle className={classes.dialogTitle}>
+                    <Typography variant="h6" className={classes.dialogText}>{title}</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    {children}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClose} className={classes.buttonClose}>{closeLabel}</Button>
+                    <Button onClick={onConfirm} className={classes.buttonConfirm}>{confirmLabel}</Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    )
+}
--- src/components/dialogs/confirmDialog.tsx
+++ src/components/dialogs/confirmDialog.tsx
@@ -1,29 +1,14 @@
 import React from "react";
 import {createStyles, makeStyles} from "@material-ui/core/styles";
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Typography} from "@material-ui/core";
+import {Grid, Typography} from "@material-ui/core";
+import {BaseConfirmDialog} from "./baseConfirmDialog";
 
-const useStyles = makeStyles((theme) =>
+const useStyles = makeStyles(() =>
     createStyles({
-        rounded_dialog: {
-            borderRadius: "12px",
-        },
-        dialogTitle: {
-            backgroundColor: "#FFCC00",
-        },
         dialogText: {
             color: "#142C45"
         },
-        buttonClose: {
-            color: "#193756",
-            backgroundColor: "#D6D6D6",
-            borderRadius: 15
-        },
-        buttonConfirm: {
-            color: "white",
-            backgroundColor: "#193756",
-            borderRadius: 15,
-            marginLeft: theme.spacing(1)
-        }
+
     })
 )
 
@@ -31,8 +16,8 @@ interface Props {
     open: boolean,
     onClose: () => void,
     onConfirm: () => void,
-    title: string | Element,
-    description: (string | Element)[],
+    title: string,
+    description: string[],
     closeLabel: string,
     confirmLabel: string
 }
@@ -44,36 +29,18 @@ export function ConfirmDialog({
                               }: Props): JSX.Element {
     const classes = useStyles();
     return (
-        <React.Fragment>
-            <Dialog fullWidth maxWidth={'sm'} className={classes.rounded_dialog}
-                    classes={{paper: classes.rounded_dialog}} open={open} onClose={onClose}>
-                <DialogTitle className={classes.dialogTitle}>
-                    {
-                        (typeof title === "string")
-                            ? <Typography variant="h6" className={classes.dialogText}>{title}</Typography>
-                            : {title}
-                    }
-                </DialogTitle>
-                <DialogContent>
-                    <Grid container direction={"column"} spacing={2}>
-                        {
-                            description.map(
-                                (item) =>
-                                    (typeof item === "string")
-                                        ? <Grid item xs>
-                                            <Typography variant="body2"
-                                                      className={classes.dialogText}>{item}</Typography>
-                                        </Grid>
-                                        : {item}
-                            )
-                        }
-                    </Grid>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onClose} className={classes.buttonClose}>{closeLabel}</Button>
-                    <Button onClick={onConfirm} className={classes.buttonConfirm}>{confirmLabel}</Button>
-                </DialogActions>
-            </Dialog>
-        </React.Fragment>
+        <BaseConfirmDialog open={open} onClose={onClose} onConfirm={onConfirm} title={title} closeLabel={closeLabel} confirmLabel={confirmLabel}>
+            <Grid container direction={"column"} spacing={2}>
+                {
+                    description.map(
+                        (item, index) =>
+                            <Grid key={index} item xs>
+                                <Typography variant="body2"
+                                            className={classes.dialogText}>{item}</Typography>
+                            </Grid>
+                    )
+                }
+            </Grid>
+        </BaseConfirmDialog>
     )
 }
