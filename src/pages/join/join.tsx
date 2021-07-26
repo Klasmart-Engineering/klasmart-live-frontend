c711c6a24089ab4463ad599cefb713166b8350a0
Dev1 MacBook Pro <axel@calmid.com>
2021-07-26 17:35:35 +0900

    Use iOS permissions check.

--- src/pages/join/join.tsx
+++ src/pages/join/join.tsx
@@ -84,8 +84,8 @@ export default function Join(): JSX.Element {
         const getCameraDevice = classtype == ClassType.LIVE;
 
         if (!permissions) {
-            requestPermissions(getCameraDevice, true);
-            // requestIosCameraPermission(getCameraDevice, true);
+            // requestPermissions(getCameraDevice, true);
+            requestIosCameraPermission(getCameraDevice, true);
         } else if(getCameraDevice) {
             refreshCameras();
         }
