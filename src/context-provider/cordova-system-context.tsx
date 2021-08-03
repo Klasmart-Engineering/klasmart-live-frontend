a01f27f470969c7a7dca991db17741690d4355b0
Hung Huynh <hung.huynh@calmid.com>
2021-08-03 22:02:02 +0700

    Added onBackQueue to handle all the back callback

--- src/context-provider/cordova-system-context.tsx
+++ src/context-provider/cordova-system-context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactChild, ReactChildren, useCallback, useState } from "react";
+import React, {createContext, ReactChild, ReactChildren, useCallback, useState} from "react";
 import { ExitDialog } from "../components/exitDialog";
 import Loading from "../components/loading";
 import useCordovaInitialize from "../cordova-initialize";
@@ -9,6 +9,12 @@ type Props = {
     history: History<unknown>
 }
 
+export type OnBackItem = {
+    id: string,
+    onBack: () => void,
+    isAutoRemove?: boolean
+}
+
 type CordovaSystemContext = {
     ready: boolean,
     isIOS: boolean,
@@ -16,12 +22,28 @@ type CordovaSystemContext = {
     devicePermissions: boolean,
     restart?: () => void,
     quit?: () => void,
+    addOnBack?: (onBackItem: OnBackItem) => void,
+    removeOnBack?: (id: string) => void
 }
 
 export const CordovaSystemContext = createContext<CordovaSystemContext>({ ready: false, devicePermissions: false, isIOS: false, isAndroid: false });
 
 export function CordovaSystemProvider({ children, history }: Props) {
     const [displayExitDialogue, setDisplayExitDialogue] = useState<boolean>(false);
+    const [onBackQueue, setOnBackQueue] = useState<OnBackItem[]>([])
+
+    const addOnBack = (onBackItem: OnBackItem) => {
+        console.log(onBackItem.id);
+        let newOnBackQueue = onBackQueue.slice();
+        newOnBackQueue.push(onBackItem);
+        setOnBackQueue(newOnBackQueue);
+    }
+
+    const removeOnBack = (id: string) => {
+        console.log(id);
+        let newOnBackQueue = onBackQueue.filter(item => item.id !== id);
+        setOnBackQueue(newOnBackQueue);
+    }
 
     const restart = useCallback(() => {
         (navigator as any).app.loadUrl("file:///android_asset/www/index.html", { wait: 0, loadingDialog: "Wait,Loading App", loadUrlTimeoutValue: 60000 });
@@ -33,9 +55,18 @@ export function CordovaSystemProvider({ children, history }: Props) {
 
     const { cordovaReady, permissions, isIOS, isAndroid } = useCordovaInitialize(false, () => {
         const isRootPage = window.location.hash.includes("/schedule") || window.location.hash === "#/";
+
         if (window.location.hash.includes("/room")) {
             restart();
         } else if (isRootPage) {
+            if(onBackQueue.length > 0){
+                const latestOnBackItem = onBackQueue[onBackQueue.length - 1];
+                latestOnBackItem.onBack();
+                if(latestOnBackItem.isAutoRemove === undefined || latestOnBackItem.isAutoRemove){
+                    removeOnBack(latestOnBackItem.id)
+                }
+                return
+            }
             if (displayExitDialogue) {
                 quit();
             } else {
@@ -51,16 +82,11 @@ export function CordovaSystemProvider({ children, history }: Props) {
         return <Loading messageId="cordova_loading" />;
     }
 
-    const Content = () => {
-        return <>
-            { children}
-            <ExitDialog visible={displayExitDialogue} onCancel={() => setDisplayExitDialogue(false)} onConfirm={() => quit()} />
-        </>
-    }
-
     return (
-        <CordovaSystemContext.Provider value={{ ready: cordovaReady, devicePermissions: permissions, restart, quit, isIOS, isAndroid }}>
-            { !cordovaReady ? <LoadingCordova /> : <Content /> }
+        <CordovaSystemContext.Provider value={{ ready: cordovaReady, devicePermissions: permissions, restart, quit, isIOS, isAndroid, addOnBack: addOnBack, removeOnBack: removeOnBack }}>
+            { !cordovaReady ? <LoadingCordova /> : '' }
+            {cordovaReady ? children : ''}
+            {cordovaReady ? <ExitDialog visible={displayExitDialogue} onCancel={() => setDisplayExitDialogue(false)} onConfirm={() => quit()} /> : ''}
         </CordovaSystemContext.Provider>
     )
 }
