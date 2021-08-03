fac44b26071dfd42ff8df5f4362cf34b0030d90f
Dev1 MacBook Pro <axel@calmid.com>
2021-08-03 20:16:48 +0900

    Remove dead code.

--- src/services/files/FileSelectService.ts
+++ src/services/files/FileSelectService.ts
@@ -44,32 +44,6 @@ export class FileSelectService implements IFileSelectService {
                 reject(error);
             });
         });
-
-        const input: HTMLInputElement = document.createElement(`input`);
-        input.type = `file`;
-        input.accept = ACCEPT_MIME_TYPES;
-
-        var selectFileProcedure = new Promise<File>((resolve, reject) => {
-            input.onchange = (e: Event) => {
-                const target = e.target as HTMLInputElement ?? undefined;
-
-                if (target && target.files?.length) {
-                    resolve(target.files[0]);
-                } else {
-                    reject("No file was selected.");
-                }
-            };
-
-            input.click();
-        });
-
-        try {
-            return await selectFileProcedure;
-        } catch (error) {
-            return Promise.reject(error);
-        } finally {
-            input.remove();
-        }
     }
 
     selectFromGallery(): Promise<File> {
