ed5a15c207c80d89d7eee6f13cf0046eed9d4ad6
Hung Huynh <hung.huynh@calmid.com>
2021-08-03 22:38:18 +0700

    Fixed condition of submission button, required at least one selected file, not required comment

--- src/pages/home-fun-study/homeFunStudyDialog.tsx
+++ src/pages/home-fun-study/homeFunStudyDialog.tsx
@@ -231,7 +231,7 @@ export function HomeFunStudyDialog() {
                 return false;
             if (studyInfo && !studyInfo.complete_assessment && (studyInfo.due_at === 0 || studyInfo?.due_at >= todayTimeStamp)) {
                 const feedback = hfsFeedbacks?.find(item => item.studyId === studyInfo.id)
-                if (feedback && (feedback.assignmentItems.length > 0 || !isBlank(feedback.comment))) {
+                if (feedback && feedback.assignmentItems.length > 0) {
                     return true;
                 }
             }
