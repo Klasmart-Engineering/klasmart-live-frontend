0e20e83c152c79c10c44c03c3b7e0cc33f24b622
Axel Wettervik <axel.wettervik@gmail.com>
2021-08-04 17:04:34 +0900

    Validate files using mime type instead of extension.

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,3 +1,8 @@
+import { ACCEPT_MIME_TYPES } from "../services/files/FileSelectService";
+
+const MIME_TYPES_REGEX = ACCEPT_MIME_TYPES.split(",")
+    .map(unescaped_mime => unescaped_mime.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
+    .map(escaped_mime => new RegExp('^' + escaped_mime.replace(/\*/g, '.*')));
 
 export function getFileExtensionFromName(fileName: string): string {
     const fileExtension = fileName.split('.').pop();
@@ -7,11 +12,16 @@ export function getFileExtensionFromName(fileName: string): string {
 export function validateFileExtension(extension: string): boolean {
     const validExtensions = ["avi", "mov", "mp4", "mp3", "wav", "jpg", "jpeg",
         "png", "gif", "bmp", "doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf"];
+
     return validExtensions.includes(getFileExtensionFromName(extension).toLowerCase());
 }
 
+export function validateFileMimeType(file: File): boolean {
+    return MIME_TYPES_REGEX.some(mime => mime.test(file.type));
+}
+
 export function validateFileType(file: File): boolean {
-    return validateFileExtension(getFileExtensionFromName(file.name));
+    return validateFileMimeType(file);
 }
 
 export function validateFileSize(file: File): boolean {
