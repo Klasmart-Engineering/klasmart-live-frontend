37d4c48d25f0962f6c5afa5d6e9f773066eb435c
Hung Huynh <hung.huynh@calmid.com>
2021-08-06 02:04:27 +0700

    fix: Every file uploaded by HFS appears as content without any extension

--- src/services/files/FileSelectService.ts
+++ src/services/files/FileSelectService.ts
@@ -1,5 +1,7 @@
 import { IFileSelectService } from "./IFileSelectService";
 /// <reference types="cordova-plugin-file" />
+/// <reference types="cordova-plugin-filepath" />
+import {FilePath} from "@ionic-native/file-path"
 
 const DESTINATION_FILE_URI = 1;
 const SOURCE_PHOTOLIBRARY = 0;
@@ -7,19 +9,19 @@ const SOURCE_CAMERA = 1;
 const SOURCE_CAMERAROLL = 2;
 const MEDIA_TYPE_ALL = 2;
 
-export const ACCEPT_MIME_TYPES = 
-    `application/pdf,` + 
-    `application/vnd.ms-powerpoint,` + 
-    `application/vnd.openxmlformats-officedocument.presentationml.presentation,` + 
-    `application/msword,` + 
+export const ACCEPT_MIME_TYPES =
+    `application/pdf,` +
+    `application/vnd.ms-powerpoint,` +
+    `application/vnd.openxmlformats-officedocument.presentationml.presentation,` +
+    `application/msword,` +
     `application/vnd.openxmlformats-officedocument.wordprocessingml.document,` +
-    `application/rtf,` + 
+    `application/rtf,` +
     `application/vnd.ms-excel,` +
     `application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,` +
-    `text/plain,` + 
-    `text/csv,` + 
-    `audio/*,` + 
-    `video/*,` + 
+    `text/plain,` +
+    `text/csv,` +
+    `audio/*,` +
+    `video/*,` +
     `image/*`;
 
 // https://github.com/cyph/cordova-plugin-chooser
@@ -33,11 +35,16 @@ export class FileSelectService implements IFileSelectService {
             throw new Error('No suitable file selected');
         }
 
-        return new Promise<File>((resolve, reject) => {
+        return new Promise<File>(async (resolve, reject) => {
             if (!uri.startsWith("file://") && !uri.startsWith("content://")) {
                 uri = "file://" + uri;
             }
-
+            if(uri.startsWith("content://")){
+                //The file get from chooser on Android has not a correct full path.
+                //Ex: content://com.android.providers.media.documents/document/image%3A15970
+                //It need to convert to native path
+                uri = await FilePath.resolveNativePath(uri)
+            }
             window.resolveLocalFileSystemURL(uri, (entry) => {
                 (entry as FileEntry).file((file) => {
                     resolve(file);
