637b24d60d8b80d0c58fb4eaf692ed7539dcfe53
Gael <gael.helle@calmid.com>
2021-08-05 16:48:16 +0900

    remove double assets

--- src/newuiupdate/assets/audio/audio.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.m4a" { const data: string; export default data; }
-declare module "*.mp3" { const data: string; export default data; }
-declare module "*.wav" { const data: string; export default data; }
-declare module "*.flac" { const data: string; export default data; }
-declare module "*.aac" { const data: string; export default data; }
