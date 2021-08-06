152f6dddc1fe8aa4102cb8721e361204e604fca2
Hung Huynh <hung.huynh@calmid.com>
2021-08-06 21:11:38 +0700

    feat: Added AssessmentService

---
+++ src/services/cms/AssessmentService.ts
@@ -0,0 +1,31 @@
+import {IAuthenticationService} from "../auth/IAuthenticationService";
+import {
+    AssessmentForStudent,
+    AssessmentsForStudentResponse,
+    AssessmentType,
+    IAssessmentService
+} from "./IAssessmentService";
+import {fetchJsonData} from "../../utils/requestUtils";
+
+export class AssessmentService implements IAssessmentService {
+    private endpoint: string;
+    private auth: IAuthenticationService;
+
+    constructor(endpoint: string, auth: IAuthenticationService) {
+        this.endpoint = endpoint;
+        this.auth = auth;
+    }
+
+    async getAssessmentsForStudent(organizationId: string, scheduleIDs: string[], type: AssessmentType, pageSize: number, page: number): Promise<AssessmentForStudent[]> {
+        const url = `${this.endpoint}/v1/assessments_for_student`;
+        const results = await fetchJsonData<AssessmentsForStudentResponse>(url, "GET", {
+            org_id: organizationId,
+            type: type,
+            schedule_ids: scheduleIDs,
+            page_size: pageSize,
+            page: page
+        }, this.auth);
+
+        return results.list;
+    }
+}
