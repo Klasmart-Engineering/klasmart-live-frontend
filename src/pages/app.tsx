3a318a17b9e04ea758596734b3a03ed8aabb6115
Gael <gael.helle@calmid.com>
2021-09-03 11:59:12 +0900

    fix: Use correct useSessionContext file

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,13 +1,12 @@
-import { LocalSessionContext } from "../providers/providers";
+import { useSessionContext } from "../providers/session-context";
 import { hasJoinedClassroomState } from "../store/layoutAtoms";
 import Join from './join/join';
 import { RoomWithContext } from './room/room-with-context';
-import React,
-{ useContext } from 'react';
+import React from 'react';
 import { useRecoilState } from "recoil";
 
 export function App () {
-    const { camera, name } = useContext(LocalSessionContext);
+    const { camera, name } = useSessionContext();
     const [ hasJoinedClassroom, setHasJoinedClassroom ] = useRecoilState(hasJoinedClassroomState);
 
     if(hasJoinedClassroom || (!name || camera !== undefined)){
