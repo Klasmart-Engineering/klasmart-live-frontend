72d239e0e7b16039166430735a5e40d1d182b1e6
Axel Wettervik <axel.wettervik@gmail.com>
2021-09-03 14:00:07 +0900

    fix: remove authentication dependency from live-session-link
    The authentication file is running code that doesn't work in app build
    once it's imported, also it's handling endpoints in a naive way. I
    think the best practice will be to get rid of it and use already
    established service for auth related functions instead.

--- src/pages/room/room-with-context.tsx
+++ src/pages/room/room-with-context.tsx
@@ -1,6 +1,7 @@
 import { Trophy } from "../../components/trophies/trophy";
 import ClassProviders from "../../providers/classProviders";
 import { LiveSessionLinkProvider } from "../../providers/live-session-link-context";
+import { useSessionContext } from "../../providers/session-context";
 import {
     classEndedState,
     classLeftState,
@@ -12,8 +13,14 @@ import React from 'react';
 import { useRecoilState } from "recoil";
 
 export function RoomWithContext (): JSX.Element {
-    const [ classLeft, setClassLeft ] = useRecoilState(classLeftState);
-    const [ classEnded, setClassEnded ] = useRecoilState(classEndedState);
+    const [ classLeft ] = useRecoilState(classLeftState);
+    const [ classEnded ] = useRecoilState(classEndedState);
+
+    const {
+        sessionId,
+        token,
+        roomId,
+    } = useSessionContext();
 
     if (classLeft) {
         return (<ClassLeft />);
@@ -24,7 +31,10 @@ export function RoomWithContext (): JSX.Element {
     }
 
     return (
-        <LiveSessionLinkProvider>
+        <LiveSessionLinkProvider
+            token={token}
+            roomId={roomId}
+            sessionId={sessionId}>
             <ClassProviders>
                 <>
                     <Room />
