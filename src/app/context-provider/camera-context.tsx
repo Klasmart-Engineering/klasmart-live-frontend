265040bd17dfcef8ba24de5121010aa71ea0ebbe
Axel Wettervik <axel.wettervik@gmail.com>
2021-09-08 18:02:19 +0900

    fix(join): use camera-context to manage video/audio stream
    Jira: KLL-1515

--- src/app/context-provider/camera-context.tsx
+++ src/app/context-provider/camera-context.tsx
@@ -1,4 +1,3 @@
-import { useSessionContext } from "../../providers/session-context";
 import useCordovaObservePause from "../platform/cordova-observe-pause";
 import React,
 {
@@ -11,19 +10,20 @@ import React,
     useState,
 } from "react";
 
-export enum FacingType {
-    USER = `user`,
-    ENVIRONMENT = `environment`
-}
-
 export interface ICameraContext {
-    stream: MediaStream | undefined;
-    facing: FacingType;
-    setFacing: React.Dispatch<React.SetStateAction<FacingType>>;
-    setAcquire: React.Dispatch<React.SetStateAction<boolean>>;
-    toggleFacing: () => void;
+    selectedAudioDeviceId: string,
+    setSelectedAudioDeviceId: React.Dispatch<React.SetStateAction<string>>,
+    availableAudioDevices: MediaDeviceInfo[],
+    selectedVideoDeviceId: string,
+    setSelectedVideoDeviceId: React.Dispatch<React.SetStateAction<string>>,
+    availableVideoDevices: MediaDeviceInfo[],
+    setAcquireCameraDevice: React.Dispatch<React.SetStateAction<boolean>>,
+    setHighQuality: React.Dispatch<React.SetStateAction<boolean>>,
+    cameraStream: MediaStream | undefined;
     refreshCameras: () => void;
-    error: boolean;
+    deviceStatus: string;
+    permissionError: boolean;
+    notFoundError: boolean;
 }
 
 const CameraContext = createContext<ICameraContext>(undefined as any);
@@ -32,19 +32,71 @@ type Props = {
     children?: ReactChild | ReactChildren | null | any;
 };
 
+export enum DeviceStatus {
+    MIC_ERROR = `mic_error`,
+    MIC_NOT_FOUND = `mic_not_found`,
+    MIC_NOT_ALLOWED = `mic_not_allowed`
+}
+
+function isMediaNotFoundError(error: any) {
+    return error.name === `NotFoundError` || error.name === `DevicesNotFoundError`;
+}
+
+function isMediaNotAllowedError(error: any) {
+    return error.name === `NotAllowedError` || error.name === `PermissionDeniedError`;
+}
+
 export const CameraContextProvider = ({ children }: Props) => {
-    const { isTeacher } = useSessionContext();
-
-    const [ acquire, setAcquire ] = useState<boolean>(false);
-    const [ facing, setFacing ] = useState<FacingType>(FacingType.USER);
+    const [ highQuality, setHighQuality ] = useState<boolean>(false);
     const [ cameraStream, setCameraStream ] = useState<MediaStream>();
-    const [ error, setError ] = useState<boolean>(false);
 
-    const toggleFacing = useCallback(() => {
-        setFacing(facing === FacingType.USER ? FacingType.ENVIRONMENT : FacingType.ENVIRONMENT);
-    }, [ facing, setFacing ]);
+    const [ notFoundError, setNotFoundError ] = useState<boolean>(false);
+    const [ permissionError, setPermissionError ] = useState<boolean>(false);
 
-    const releaseCameraDevice = useCallback(() => {
+    const [ selectedAudioDeviceId, setSelectedAudioDeviceId ] = useState<string>(``);
+    const [ selectedVideoDeviceId, setSelectedVideoDeviceId ] = useState<string>(``);
+
+    const [ availableAudioDevices, setAvailableAudioDevices ] = useState<MediaDeviceInfo[]>([]);
+    const [ availableVideoDevices, setAvailableVideoDevices ] = useState<MediaDeviceInfo[]>([]);
+
+    const [ deviceStatus, setDeviceStatus ] = useState<string>(``);
+
+    const [ acquireCameraDevice, setAcquireCameraDevice ] = useState<boolean>(true);
+
+    const requestPermissions = useCallback(async () => {
+        console.log(`request media permissions`);
+        return await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        }).then(stream => {
+            stream.getTracks().forEach(track => {
+                track.stop();
+            })
+        });
+    }, []);
+
+    const getAvailableDevices = useCallback(async () => {
+        try {
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            console.log(`enumerated ${devices?.length} devices`);
+
+            setAvailableVideoDevices(devices?.filter(d => d.kind === `videoinput`) ?? []);
+            setAvailableAudioDevices(devices?.filter(d => d.kind === `audioinput`) ?? []);
+
+            setPermissionError(false);
+            setNotFoundError(false);
+        } catch (error) {
+            console.error(`error enumerating media devices: ${error}`);
+
+            if (isMediaNotAllowedError(error)) {
+                setPermissionError(true);
+            } else {
+                setNotFoundError(true);
+            }
+        }
+    }, []);
+
+    const releaseCameraStream = useCallback(() => {
         if (!cameraStream) return;
 
         cameraStream.getTracks()
@@ -52,92 +104,128 @@ export const CameraContextProvider = ({ children }: Props) => {
         setCameraStream(undefined);
     }, [ cameraStream, setCameraStream ]);
 
-    const reacquireCameraDevice = useCallback(() => {
-        if (!acquire) {
-            setCameraStream(undefined);
-            setError(false);
-            return;
-        }
-
+    const acquireCameraStream = useCallback(() => {
         if (!navigator.mediaDevices) {
             setCameraStream(undefined);
-            setError(true);
+            setNotFoundError(true);
             return;
         }
 
-        const facingMode = facing === FacingType.USER ? `user` : `environment`;
+        console.log(`acquire camera stream: videoDevice ${selectedVideoDeviceId}, audioDevice ${selectedAudioDeviceId}`);
+
+        const audioConstraints: MediaTrackConstraints | boolean = {
+            deviceId: selectedAudioDeviceId,
+        };
+
+        const videoConstraints: MediaTrackConstraints | boolean = acquireCameraDevice ? {
+            deviceId: selectedVideoDeviceId,
+            width: {
+                max: 720,
+                ideal: highQuality ? 720 : 180,
+            },
+            height: {
+                max: 540,
+                ideal: highQuality ? 540 : 96,
+            },
+            frameRate: {
+                max: 15,
+                ideal: highQuality ? 15 : 12,
+            },
+        } : false;
 
         navigator.mediaDevices.getUserMedia({
-            video: {
-                facingMode,
-                width: {
-                    max: 720,
-                    ideal: isTeacher?720:180,
-                },
-                height: {
-                    max: 540,
-                    ideal: isTeacher?540:96,
-                },
-                frameRate: {
-                    max: 15,
-                    ideal: isTeacher?15:12,
-                },
-            },
-            audio: true,
-        })
-            .then((s) => { setError(false); setCameraStream(s); })
-            .catch((e) => { setError(true); console.error(e); });
+            audio: audioConstraints,
+            video: videoConstraints,
+        }).then(stream => {
+            setDeviceStatus(``);
+            setNotFoundError(false);
+            setPermissionError(false);
+            setCameraStream(stream);
+        }).catch(error => {
+            console.error(`getUserMedia error: ${error}`);
 
-    }, [
-        facing,
-        releaseCameraDevice,
-        acquire,
-    ]);
+            if (selectedVideoDeviceId) {
+                setPermissionError(true);
+            } else {
+                if (isMediaNotFoundError(error)) {
+                    setDeviceStatus(DeviceStatus.MIC_NOT_FOUND);
+                    setPermissionError(false);
+                    setNotFoundError(true);
+                } else if (isMediaNotAllowedError(error)) {
+                    setDeviceStatus(DeviceStatus.MIC_NOT_ALLOWED);
+                    setPermissionError(true);
+                    setNotFoundError(false);
+                } else {
+                    setDeviceStatus(DeviceStatus.MIC_ERROR);
+                }
+            }
+        });
+    }, []);
 
-    const refreshCameras = useCallback(() => {
-        setAcquire(true);
+    const refreshCameras = useCallback(async () => {
+        console.log(`refreshing cameras`);
+        
+        try {
+            await requestPermissions();
+            await getAvailableDevices();
 
-        releaseCameraDevice();
-        reacquireCameraDevice();
+            releaseCameraStream();
+            acquireCameraStream();
+        } catch(error) {
+            console.error(`no camera permissions: ${error}`);
+            setPermissionError(true);
+        }
     }, []);
 
     useEffect(() => {
-        reacquireCameraDevice();
+        acquireCameraStream();
 
         return () => {
-            releaseCameraDevice();
+            releaseCameraStream();
         };
-    }, [ facing, acquire ]);
+    }, [ selectedAudioDeviceId, selectedVideoDeviceId, acquireCameraDevice ]);
+
+    useEffect(() => {
+        refreshCameras();
+    }, []);
 
     useEffect(() => {
         if (!navigator.mediaDevices) { return; }
 
-        const onDeviceChange = () => { reacquireCameraDevice(); };
+        const onDeviceChange = () => {
+            refreshCameras();
+        };
 
         navigator.mediaDevices.addEventListener(`devicechange`, onDeviceChange);
         return () => {
             navigator.mediaDevices.removeEventListener(`devicechange`, onDeviceChange);
         };
-    }, [ reacquireCameraDevice ]);
+    }, [ refreshCameras, refreshCameras ]);
 
     const onPauseStateChanged = useCallback((paused: boolean) => {
         if (paused) {
-            releaseCameraDevice();
+            releaseCameraStream();
         } else {
-            reacquireCameraDevice();
+            acquireCameraStream();
         }
-    }, [ releaseCameraDevice, reacquireCameraDevice ]);
+    }, [ releaseCameraStream, acquireCameraStream ]);
 
     useCordovaObservePause(onPauseStateChanged);
 
     return (
         <CameraContext.Provider value={{
-            stream: cameraStream,
-            facing,
-            setFacing,
-            toggleFacing,
-            setAcquire,
-            error,
+            selectedVideoDeviceId,
+            setSelectedVideoDeviceId,
+            availableVideoDevices,
+            selectedAudioDeviceId,
+            setSelectedAudioDeviceId,
+            availableAudioDevices,
+            setAcquireCameraDevice,
+            setHighQuality,
+            cameraStream,
+            notFoundError,
+            permissionError,
+            deviceStatus,
             refreshCameras,
         }}>
             {children}
