6b34728df67cdd621092bf2d67e08363278c3a42
Hung Huynh <hung.huynh@calmid.com>
2021-09-13 14:10:55 +0700

    fix: Moved saveDataBlobToFile and downloadDataBlob into Utils
    Jira: KLL-1486

--- src/app/utils/fileUtils.ts
+++ src/app/utils/fileUtils.ts
@@ -7,17 +7,17 @@ const MIME_TYPES_REGEX = ACCEPT_MIME_TYPES.split(`,`)
     .map(unescapedMime => unescapedMime.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`))
     .map(escapedMime => new RegExp(`^` + escapedMime.replace(/\*/g, `.*`)));
 
-export function getFileExtensionFromName (fileName: string): string {
+export function getFileExtensionFromName(fileName: string): string {
     const fileExtension = fileName.split(`.`).pop();
     return fileExtension ? fileExtension : ``;
 }
 
-export function getFileExtensionFromType (fileType: string): string {
+export function getFileExtensionFromType(fileType: string): string {
     const fileExtension = MIME_TO_EXTENSION.get(fileType);
     return fileExtension ? fileExtension : ``;
 }
 
-export function validateFileExtension (extension: string): boolean {
+export function validateFileExtension(extension: string): boolean {
     const validExtensions = [
         `avi`,
         `mov`,
@@ -41,18 +41,55 @@ export function validateFileExtension (extension: string): boolean {
     return validExtensions.includes(getFileExtensionFromName(extension).toLowerCase());
 }
 
-export function validateFileMimeType (file: File): boolean {
+export function validateFileMimeType(file: File): boolean {
     return MIME_TYPES_REGEX.some(mime => mime.test(file.type));
 }
 
-export function validateFileType (file: File): boolean {
+export function validateFileType(file: File): boolean {
     return validateFileMimeType(file);
 }
 
-export function validateFileSize (file: File): boolean {
+export function validateFileSize(file: File): boolean {
     return bytesToMegaBytes(file.size) <= 100;
 }
 
-export function bytesToMegaBytes (bytes: number): number {
+export function bytesToMegaBytes(bytes: number): number {
     return bytes / (1024 * 1024);
 }
+
+export function getNameWithoutExtension(fileName: string): string {
+    return fileName.replace(/\.[^/.]+$/, "");
+}
+
+export function convertFileNameToUnderscores(fileName: string): string {
+    return `${getNameWithoutExtension(fileName).replace(/([^a-z0-9])/gi, '_')}.${getFileExtensionFromName(fileName)}`
+}
+
+export function saveDataBlobToFile (blob: Blob, targetDirectory: string, fileName: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        (window as any).resolveLocalFileSystemURL(targetDirectory, (entry: any) => {
+            const newFileName = convertFileNameToUnderscores(fileName);
+            entry.getFile( newFileName, { create: true, exclusive: false }, (fileEntry: any) => {
+                fileEntry.createWriter((writer: any) => {
+                    writer.onwriteend = () => {
+                        resolve(fileEntry.toURL());
+                    };
+
+                    writer.onerror = () => {
+                        console.error('could not write file: ', writer.error);
+                        reject(writer.error);
+                    }
+
+                    writer.write(blob);
+                });
+            }, (error: any) => {
+                console.error('could not create file: ', error);
+                reject(error);
+            });
+        }, (error: any) => {
+            console.error('could not retrieve directory: ', error);
+            reject(error);
+        })
+    });
+}
+
