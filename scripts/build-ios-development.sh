abdb58ee7e107f2ff45f24960b18605fc0ea37ee
Hung Huynh <hung.huynh@calmid.com>
2021-09-15 16:57:10 +0700

    feat: iOS CORS workaround(native)
    The iOS build progress need to be insert two line allowFileAccessFromFileUrls and allowUniversalAccessFromFileUrls into the CDVWebViewEngine.m. This commit will make automatically.
    
    Jira: KLL-1494

--- scripts/build-ios-development.sh
+++ scripts/build-ios-development.sh
@@ -9,7 +9,7 @@ export IS_CORDOVA_BUILD="true"
 
 # Clean Build
 rm -rf ./www
-# rm -rf ./platforms/android
+#rm -rf ./platforms/ios
 
 # Ensure packages is installed
 npm install
@@ -22,5 +22,27 @@ npm run build:app
 
 # Prepare android platform
 cordova prepare ios
+
+# Modify the CDVWebViewEngine to  set allowFileAccessFromFileURLs and allowUniversalAccessFromFileURLs to TRUE
+webViewEngineFilePath='./platforms/ios/CordovaLib/Classes/Private/Plugins/CDVWebViewEngine/CDVWebViewEngine.m'
+wkWebViewConfigurationLine='WKWebViewConfiguration\* configuration = \[\[WKWebViewConfiguration alloc\] init\]\;'
+allowFileAccessFromFileURLsLine='\[configuration.preferences setValue\:\@TRUE forKey\:\@\"allowFileAccessFromFileURLs\"\]\;'
+allowUniversalAccessFromFileURLsLine='\[configuration setValue\:\@\"TRUE\" forKey\:\@\"allowUniversalAccessFromFileURLs\"\]\;'
+
+if ! grep -q "allowUniversalAccessFromFileURLs" "$webViewEngineFilePath"; then
+  printf "\e[1;33m\nWarning: The CDVWebViewEngine need to be modified that set\e[0m \e[1;34mallowUniversalAccessFromFileURLs\e0m \e[1;33mto TRUE.\e[0m\n"
+  echo -e "Starting modify the CDVWebViewEngine at:\n$webViewEngineFilePath"
+  newCDVWebViewEngine=$(sed "s/$wkWebViewConfigurationLine/$wkWebViewConfigurationLine\n\t$allowUniversalAccessFromFileURLsLine/" $webViewEngineFilePath)
+  echo "$newCDVWebViewEngine" > $webViewEngineFilePath
+  printf "\e[1;32mThe CDVWebViewEngine was modified successful.\e[0m\n"
+fi
+
+if ! grep -q "allowFileAccessFromFileURLs" "$webViewEngineFilePath"; then
+  printf "\e[1;33m\nWarning: The CDVWebViewEngine need to be modified that set\e[0m \e[1;34mallowFileAccessFromFileURLs\e0m \e[1;33mto TRUE.\e[0m\n"
+  echo -e "Starting modify the CDVWebViewEngine at:\n$webViewEngineFilePath"
+  newCDVWebViewEngine=$(sed "s/$wkWebViewConfigurationLine/$wkWebViewConfigurationLine\n\t$allowFileAccessFromFileURLsLine/" $webViewEngineFilePath)
+  echo "$newCDVWebViewEngine" > $webViewEngineFilePath
+  printf "\e[1;32mThe CDVWebViewEngine was modified successful.\e[0m\n"
+fi
 
 open platforms/ios/KidsLoop.xcworkspace
