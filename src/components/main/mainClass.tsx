06933edf973c33347303b58c28dfeeb8578ceeda
Gael <gael.helle@calmid.com>
2021-10-19 10:16:15 +0900

    fix: add Safe Area for Study view

--- src/components/main/mainClass.tsx
+++ src/components/main/mainClass.tsx
@@ -3,19 +3,28 @@ import { WBToolbarContainer } from "@/components/classContent/WBToolbar";
 import { useSessionContext } from "@/providers/session-context";
 import { ClassType } from "@/store/actions";
 import { showEndStudyState } from "@/store/layoutAtoms";
+import { makeStyles } from "@material-ui/core";
 import React,
 { useContext } from "react";
 import { useRecoilState } from "recoil";
 
+const useStyles = makeStyles((theme: Theme) => ({
+    safeArea: {
+        marginLeft: `env(safe-area-inset-left)`,
+        marginRight: `env(safe-area-inset-right)`,
+    },
+}));
+
 function MainClass () {
+    const { safeArea } = useStyles();
     const { classType } = useSessionContext();
     const [ showEndStudy, setShowEndStudy ] = useRecoilState(showEndStudyState);
 
     return (
-        <>
+        <div className={safeArea}>
             <ClassContent />
             {!showEndStudy &&  <WBToolbarContainer useLocalDisplay={classType !== ClassType.LIVE} />}
-        </>
+        </div>
     );
 }
 
