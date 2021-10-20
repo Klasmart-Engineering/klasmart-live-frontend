056f8da3bbb253869ee5440bd60c2ce2ae29ecf8
Henrik Wassdahl <henrik.wassdahl@kidsloop.live>
2021-10-20 04:11:26 +0000

    feat(translation): add thai translations
    Merged in lokalise-2021-10-15_07-40-34 (pull request #529)
    
    Lokalise: Translations update
    
    Approved-by: Gaël HELLÉ
    Approved-by: Axel Wettervik

--- src/app/localization/localeCodes.ts
+++ src/app/localization/localeCodes.ts
@@ -3,11 +3,13 @@ import indonesian from "./id.json";
 import korean from "./ko.json";
 import vietnamese from "./vi.json";
 import chinese from "./zh_CN.json";
+import thai from "./th.json";
 import englishWeb from "@/localization/en.json";
 import indonesianWeb from "@/localization/id.json";
 import koreanWeb from "@/localization/ko.json";
 import vietnameseWeb from "@/localization/vi.json";
 import chineseWeb from "@/localization/zh_CN.json";
+import thaiWeb from "@/localization/th.json";
 import {
     createIntl,
     createIntlCache,
@@ -19,6 +21,7 @@ export const localeCodes = [
     `zh-CN`,
     `vi`,
     `id`,
+    `th`,
 ];
 
 const intlCache = createIntlCache();
@@ -69,6 +72,14 @@ export function getIntl (locale: string) {
             messages: {
                 ...indonesian,
                 ...indonesianWeb,
+            },
+        }, intlCache);
+    case `th`:
+        return createIntl({
+            locale: `th`,
+            messages: {
+                ...thai,
+                ...thaiWeb,
             },
         }, intlCache);
     default: return fallbackLocale;
