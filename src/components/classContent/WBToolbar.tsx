d6e8988aff7b711c09803ea6034191ba55b13777
Dat Doan <dat.doan@kidsloop.live>
2021-11-03 07:11:51 +0000

    fix: set permissions.allowCreateShapes true when classType is CLASSES
    jira: KLL-1162
    
    * update: change code / enable permission in WBToolbar instead of Whiteboard
    
    jira: KLL-1162
    
    * update: change the condition, allow permission for study/class as well
    
    jira: KLL-1162
    
    
    Approved-by: Axel Wettervik
    Approved-by: Gaël HELLÉ

--- src/components/classContent/WBToolbar.tsx
+++ src/components/classContent/WBToolbar.tsx
@@ -65,7 +65,7 @@ export function WBToolbarContainer ({ useLocalDisplay } : { useLocalDisplay?: bo
             const permissions = getPermissions(sessionId);
             const newPermissions = {
                 ...permissions,
-                allowCreateShapes: !permissions.allowCreateShapes,
+                allowCreateShapes: true,
             };
             setPermissions(sessionId, newPermissions);
         }
