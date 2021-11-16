378ca5e38a3d9f16640c2b6a6616c601b1de24dd
Kylie Judd <kylie.judd@kidsloop.live>
2021-11-16 07:07:15 +0000

    Merged in feature/deployment_config (pull request #596)
    adding deployment script
    
    * adding deployment script
    
    * Merged master into feature/deployment_config
    * updating comments
    
    * updating pdf version to jpeg
    
    
    Approved-by: Axel Wettervik
    Approved-by: Gaël HELLÉ

--- deploy-pk.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# deployment for kidsloop.co.pk
-# make sure you run git lfs install && git lfs pull
-npm ci --no-audit --no-progress
-cp deploy/config/pk/.env.prod ./.env
-npm run build
-aws s3 sync dist s3://klpk-prod-live/$(date "+%Y%m%d") # backup
-aws s3 sync dist s3://klpk-prod-live/latest --delete
-aws cloudfront create-invalidation --paths "/*" --distribution-id E2TEBPLIWZWOVS
