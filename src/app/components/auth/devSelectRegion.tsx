38907ae6acbbdbaa7558d15b9d5e44a1694e17b3
Gaël Hellé Hellé <gael.helle@kidsloop.live>
2021-12-02 04:46:40 +0000

    style: a bit of css for dev select region
    Merged in ui/dev-select-region-ui-adjustment (pull request #664)
    
    * style: a bit of css for dev select region
    
    * style: duplicated classes
    
    * chore: separate devSelectRegion file
    
    * chore: move devSelectRegion file
    
    
    Approved-by: Henrik Wassdahl
    Approved-by: Axel Wettervik

---
+++ src/app/components/auth/devSelectRegion.tsx
@@ -0,0 +1,79 @@
+import { useCordovaSystemContext } from "@/app/context-provider/cordova-system-context";
+import { selectedRegionState } from "@/app/model/appModel";
+import { THEME_COLOR_PRIMARY_DEFAULT } from "@/config";
+import {
+    Button,
+    createStyles,
+    makeStyles,
+} from "@material-ui/core";
+import clsx from "clsx";
+import React,
+{ useCallback } from "react";
+import { useRecoilState } from "recoil";
+
+const useStyles = makeStyles((theme) => createStyles({
+    regionButton: {
+        boxShadow: `none`,
+        backgroundColor: theme.palette.grey[200],
+        marginBottom: theme.spacing(1),
+        "&:hover": {
+            boxShadow: `none`,
+            backgroundColor: theme.palette.grey[200],
+        },
+    },
+    regionButtonActive: {
+        backgroundColor: THEME_COLOR_PRIMARY_DEFAULT,
+        color: theme.palette.common.white,
+    },
+}));
+
+export function DevSelectRegion (){
+    const classes = useStyles();
+
+    const regions = [
+        {
+            id: `auth.alpha.kidsloop.net`,
+            name: `Alpha`,
+        },
+        {
+            id: `auth.stage.kidsloop.live`,
+            name: `Staging`,
+        },
+        {
+            id: `auth.kidsloop.live`,
+            name: `Production`,
+        },
+    ];
+
+    const [ selectedRegion, setSelectedRegion ] = useRecoilState(selectedRegionState);
+    const { restart } = useCordovaSystemContext();
+
+    const selectRegionWithId = useCallback((regionId: string) => {
+        setSelectedRegion({
+            ...selectedRegion,
+            regionId,
+        });
+
+        if (restart) {
+            restart();
+        }
+    }, [ selectedRegion, restart ]);
+
+    return (
+        <>
+            {regions.map(region =>
+                <Button
+                    key={region.id}
+                    fullWidth
+                    variant="contained"
+                    size="large"
+                    className={clsx(classes.regionButton, {
+                        [classes.regionButtonActive]: region.id === selectedRegion.regionId,
+                    })}
+                    onClick={() => { selectRegionWithId(region.id); }}
+                >
+                Select region: {region.name}
+                </Button>)}
+        </>
+    );
+}
