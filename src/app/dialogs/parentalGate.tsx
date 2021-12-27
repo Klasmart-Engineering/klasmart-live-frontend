ec2c1f405ec55af6fa71d9ffc872e5524307b8c4
Nguyen Minh Duc <duc.nguyenminh1@nashtechglobal.com>
2021-12-27 09:10:06 +0000

    feat: implement mechanism for parent lock screen
    * feat: implement mechanism for parent lock screen
    
    
    closes: KLL-2150
    Approved-by: Axel Wettervik
    Approved-by: Phat Ho Hoang
    Approved-by: Gaël Hellé Hellé

--- src/app/dialogs/parentalGate.tsx
+++ src/app/dialogs/parentalGate.tsx
@@ -1,190 +1,270 @@
 import { useDeviceOrientationValue } from "@/app/model/appModel";
-import { THEME_COLOR_SECONDARY_DEFAULT } from "@/config";
+import TopBackground from "@/assets/img/background_parental_lock.svg";
+import BadaCharacter from "@/assets/img/bada_character.svg";
+import { CaptchaLogic } from "@/components/captchaLogic";
+import StyledIcon from "@/components/styled/icon";
+import {
+    THEME_COLOR_BACKGROUND_ICON_PARENTAL_LOCK,
+    THEME_COLOR_BACKGROUND_PAPER,
+    THEME_COLOR_BACKGROUND_PARENTAL_LOCK,
+    THEME_COLOR_ICON_PARENTAL_LOCK,
+} from "@/config";
+import { useWindowSize } from "@/utils/viewport";
 import {
     createStyles,
-    Input,
     makeStyles,
     Theme,
-    useTheme,
 } from "@material-ui/core";
 import red from "@material-ui/core/colors/red";
 import Grid from "@material-ui/core/Grid/Grid";
 import Typography from "@material-ui/core/Typography/Typography";
+import { LockClosed as ParentLockIcon } from "@styled-icons/heroicons-solid/LockClosed";
+import { Close as CloseIcon } from "@styled-icons/material/Close";
+import clsx from "clsx";
 import React,
 {
     useEffect,
-    useMemo,
-    useRef,
     useState,
 } from 'react';
 import { FormattedMessage } from "react-intl";
 
 interface Props {
     onCompleted: () => void;
-    message?: string;
-    showHeader?: boolean;
+    isWelcomeScreen?: boolean;
+    setClosedDialog?: () => void;
 }
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-    titleText: {
-        color: THEME_COLOR_SECONDARY_DEFAULT,
+    parentLockIcon:{
+        display: `inline-block`,
+        background: THEME_COLOR_BACKGROUND_ICON_PARENTAL_LOCK,
+        borderRadius: `50%`,
+        padding: 10,
+        color: THEME_COLOR_ICON_PARENTAL_LOCK,
+    },
+    container: {
+        flex: 1,
+        width: `100%`,
+        height: `100%`,
+        position: `relative`,
+        backgroundColor: THEME_COLOR_BACKGROUND_PARENTAL_LOCK,
+    },
+    bgImage: {
+        width: `100%`,
+    },
+    contentWrapper:{
+        position: `relative`,
+        height: `100%`,
+    },
+    contentWrapperPortraitMode: {
+        position: `absolute`,
+        top: `27%`,
+        bottom: 0,
+
+        [theme.breakpoints.up(`sm`)]: {
+            top: `45%`,
+        },
+    },
+    contentWrapperSmallHeight: {
+        top: `30%`,
+    },
+    content: {
+        flex: 1,
+        backgroundColor: THEME_COLOR_BACKGROUND_PAPER,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+    },
+    contentPortraitMode:{
+        position: `absolute`,
+        bottom: 0,
+        zIndex: 5,
+        top: 67,
+        padding: theme.spacing(3),
+    },
+    landscapeLayout: {
+        padding: 0,
+        [theme.breakpoints.up(`md`)]: {
+            justifyContent: `center`,
+        },
+    },
+    contentWidth:{
+        width: `40%`,
+    },
+    contentWidthPortraitMode:{
+        width: `85%`,
+    },
+    characterImg: {
+        zIndex: 10,
+    },
+    headerText: {
         fontWeight: theme.typography.fontWeightBold as number,
     },
     errorText: {
         color: red[500],
     },
+    dialogCloseIconButton:{
+        borderRadius: `50%`,
+        width: `2rem`,
+        height: `2rem`,
+        background: theme.palette.common.white,
+        display: `flex`,
+        alignItems: `center`,
+        justifyContent: `center`,
+        position: `fixed`,
+        top: 0,
+        left: 0,
+        margin: theme.spacing(2),
+        zIndex: 10,
+    },
+    padding: {
+        padding: theme.spacing(3),
+    },
+    paddingSmallHeight: {
+        padding: theme.spacing(2),
+    },
+    paddingTopSmallHeight: {
+        padding: 0,
+        paddingTop: theme.spacing(2),
+    },
 }));
 
 export function ParentalGate ({
     onCompleted,
-    message,
-    showHeader,
+    isWelcomeScreen,
+    setClosedDialog,
 }: Props): JSX.Element {
-    const theme = useTheme();
     const classes = useStyles();
+    const { height } = useWindowSize();
     const deviceOrientation = useDeviceOrientationValue();
 
-    const [ challenge1 ] = useState<number>(Math.ceil(Math.random() * 50));
-    const [ challenge2 ] = useState<number>(Math.ceil(Math.random() * 50));
-
-    const correctAnswer = useMemo<number>(() => {
-        return challenge1 + challenge2;
-    }, [ challenge1, challenge2 ]);
-
+    const [ header, setHeader ] = useState<string>(`parentalGate.title`);
     const [ isShowError, setShowError ]  = useState<boolean>(false);
+    const [ showParentCaptcha, setShowParentCaptcha ] = useState<boolean>(true);
+    const [ isPortrait, setPortrait ] = useState<boolean>(deviceOrientation.includes(`portrait`));
+    const [ isSmallHeight ] = useState<boolean>(height <= 680);
 
-    const inputRef = useRef<HTMLInputElement>();
+    useEffect(() => {
+        setHeader(isWelcomeScreen ? `parentalGate.title` : `forParents.title`);
+    }, [ isWelcomeScreen ]);
 
-    const handleChangeInputText = (inputValue:string): void => {
-        if(!inputRef.current) return;
-
-        const answer = Number(inputValue);
-        if(answer === correctAnswer){
+    useEffect(() => {
+        if(!showParentCaptcha){
             onCompleted();
             return;
         }
-
-        if(inputValue === ``){
-            setShowError(false);
-            return;
-        }
-
-        if(inputValue.length >= correctAnswer.toString().length){
-            setShowError(true);
-        }
-    };
+    }, [ showParentCaptcha ]);
 
     useEffect(() => {
-        if (!inputRef.current) return;
-        inputRef.current.focus();
-
-        const Keyboard = (window as any).Keyboard;
-        if (!Keyboard) return;
-        Keyboard.show();
-    }, []);
+        setPortrait(deviceOrientation.includes(`portrait`));
+    }, [ deviceOrientation ]);
 
     return (
         <Grid
             container
             item
             direction="column"
-            justifyContent="center"
+            justifyContent="flex-start"
             alignItems="center"
-            wrap="nowrap"
-            style={{
-                overflow: `scroll`,
-                flex: 1,
-                height: deviceOrientation === `portrait` ?  `100%` : `auto`,
-                backgroundColor: `white`,
-                padding: theme.spacing(2),
-            }}
-        >
-            {showHeader && (
-                <Grid item>
-                    <Typography
-                        gutterBottom
-                        variant="h3"
-                        align="center"
-                        className={classes.titleText}>
-                        <FormattedMessage id="parentalGate.title" />
-                    </Typography>
+            className={classes.container}>
+            {!isWelcomeScreen && (
+                <Grid
+                    item
+                    onClick={setClosedDialog}>
+                    <div className={classes.dialogCloseIconButton}>
+                        <StyledIcon
+                            icon={<CloseIcon />}
+                            size={`large`}
+                        />
+                    </div>
+                </Grid>)}
+            {isPortrait && (
+                <Grid
+                    item
+                    className={classes.bgImage}>
+                    <img
+                        src={TopBackground}
+                        width="100%"
+                        height="100%"/>
                 </Grid>)}
             <Grid
+                container
                 item
-                style={{
-                    width: `75%`,
-                    marginTop: theme.spacing(deviceOrientation === `portrait` ? 3 : 1),
-                    marginBottom: theme.spacing(deviceOrientation === `portrait` ? 3 : 1),
-                }}>
-                <Typography
-                    gutterBottom
-                    variant="subtitle1"
-                    align="center">
-                    { message ? message : <FormattedMessage id="parentalGate.body.default" /> }
-                </Typography>
-            </Grid>
-            <Grid
-                item
-                style={{
-                    height: 10,
-                }}>
-                {isShowError && (
-                    <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        align="center"
-                        className={classes.errorText}>
-                        <FormattedMessage id="parentalGate.error.incorrect" />
-                    </Typography>)}
-            </Grid>
-            <Grid item>
+                direction="column"
+                alignItems="center"
+                className={clsx(classes.contentWrapper, {
+                    [classes.contentWrapperPortraitMode] : isPortrait,
+                    [classes.contentWrapperSmallHeight]: isPortrait && isSmallHeight,
+                })}>
+                {isPortrait && (
+                    <Grid
+                        item
+                        className={classes.characterImg}
+                    >
+                        <img
+                            src={BadaCharacter} />
+                    </Grid>)}
                 <Grid
                     container
                     item
-                    direction="row"
-                    justifyContent="center"
+                    direction="column"
+                    justifyContent="flex-start"
                     alignItems="center"
-                    style={{
-                        flex: 1,
-                        margin: theme.spacing(1),
-                    }}>
+                    className={clsx(classes.content, {
+                        [classes.contentPortraitMode] : isPortrait,
+                        [classes.landscapeLayout]: !isPortrait,
+                        [classes.paddingTopSmallHeight] : isSmallHeight && isPortrait,
+                    })}>
                     <Grid
                         item
-                        style={{
-                            margin: theme.spacing(1),
-                        }}>
-                        <FormattedMessage
-                            id="parentalGate.challenge"
-                            values={{
-                                number1: challenge1,
-                                number2: challenge2,
-                            }} />
+                        className={classes.parentLockIcon}>
+                        <ParentLockIcon size="4rem"  />
+                    </Grid>
+                    <Grid item>
+                        <Typography
+                            gutterBottom
+                            variant="h5"
+                            align="center"
+                            color="primary"
+                            className={clsx(classes.headerText, classes.padding, {
+                                [classes.paddingSmallHeight] : isSmallHeight,
+                            })}>
+                            <FormattedMessage id={header} />
+                        </Typography>
                     </Grid>
                     <Grid
                         item
-                        style={{
-                            margin: theme.spacing(1),
-                        }}>
-                        <Input
-                            autoFocus
-                            inputRef={inputRef}
-                            type="number"
-                            inputProps={{
-                                min:`0`,
-                                max:`99`,
-                                size:`1`,
-                                maxLength:`2`,
-                                inputMode:`numeric`,
-                                pattern:`[0-9]*`,
-                                style: {
-                                    textAlign: `center`,
-                                },
-                            }}
-                            className={isShowError ? classes.errorText : ``}
-                            onBlur={() => inputRef.current?.focus()}
-                            onChange={(e) => {
-                                handleChangeInputText(e.target.value);
-                            }} />
+                        className={clsx(classes.contentWidth, {
+                            [classes.contentWidthPortraitMode] : isPortrait,
+                        })}>
+                        <Typography
+                            gutterBottom
+                            color="primary"
+                            variant="subtitle1"
+                            align="center">
+                            <FormattedMessage id="forParents.body" />
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        item
+                        className={clsx(classes.contentWidth, {
+                            [classes.paddingSmallHeight] : !isSmallHeight && isPortrait,
+                            [classes.contentWidthPortraitMode] : isPortrait,
+                        })}>
+                        <CaptchaLogic
+                            setError={setShowError}
+                            setShowParentCaptcha={setShowParentCaptcha} />
+                    </Grid>
+                    <Grid item>
+                        {isShowError && (
+                            <Typography
+                                gutterBottom
+                                variant="subtitle1"
+                                align="center"
+                                className={clsx(classes.errorText, classes.padding, {
+                                    [classes.paddingTopSmallHeight] : isSmallHeight,
+                                })}>
+                                <FormattedMessage id="forParents.incorrect" />
+                            </Typography>)}
                     </Grid>
                 </Grid>
             </Grid>
---
+++ src/components/captchaLogic.tsx
@@ -0,0 +1,147 @@
+import {
+    THEME_COLOR_BACKGROUND_PAPER,
+    THEME_COLOR_PRIMARY_DEFAULT,
+} from "@/config";
+import {
+    createStyles,
+    Grid,
+    makeStyles,
+    Theme,
+    Typography,
+} from "@material-ui/core";
+import clsx from "clsx";
+import React,
+{
+    useEffect,
+    useState,
+} from "react";
+
+interface Props {
+    setError: (props:boolean) => void;
+    setShowParentCaptcha: (props:boolean) => void;
+    isUsedForEndClass?: boolean;
+}
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    parentChecker:{
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    },
+    parentCheckerItem: {
+        color: THEME_COLOR_BACKGROUND_PAPER,
+        background: THEME_COLOR_PRIMARY_DEFAULT,
+        justifyContent: `center`,
+        display: `flex`,
+        alignItems: `center`,
+        borderRadius: `50%`,
+        cursor: `pointer`,
+        margin: `0 10px`,
+    },
+    smallChecker: {
+        width: 50,
+        height: 50,
+    },
+    largeChecker: {
+        width: 70,
+        height: 70,
+    },
+    parentCheckerItemActive:{
+        opacity: 0.4,
+    },
+}));
+
+function CaptchaLogic ({
+    setError, setShowParentCaptcha, isUsedForEndClass,
+}:Props){
+    const classes = useStyles();
+    const [ checkNumbers, setCheckNumbers ] = useState<any[]>([]);
+
+    const generateRandomNumbers = () => {
+        const randomNumbers:any[] = [];
+        while(randomNumbers.length < 3){
+            const number = Math.floor(Math.random() * 100) + 1;
+            if(randomNumbers.indexOf(number) === -1) randomNumbers.push(number);
+        }
+
+        setCheckNumbers([
+            {
+                value: randomNumbers[0],
+                checked: false,
+                selected: 0,
+            },
+            {
+                value: randomNumbers[1],
+                checked: false,
+                selected: 0,
+            },
+            {
+                value: randomNumbers[2],
+                checked: false,
+                selected: 0,
+            },
+        ]);
+    };
+
+    const handleSelectNumber = (value:number, index: number) => {
+        const updatedNumbers = [ ...checkNumbers ];
+        updatedNumbers[index] = {
+            ...checkNumbers[index],
+            checked: true,
+            selected: updatedNumbers.filter(number => number.checked === true).length,
+        };
+        setCheckNumbers(updatedNumbers);
+        setError(false);
+    };
+
+    useEffect(() => {
+        generateRandomNumbers();
+    }, []);
+
+    useEffect(() => {
+        if(checkNumbers.length){
+            const findUnchecked = checkNumbers.find(number => number.checked === false);
+
+            if(findUnchecked === undefined){
+                const selectedNumbers = checkNumbers
+                    .sort((a, b) => a.selected - b.selected)
+                    .map(number => number.value);
+
+                const isValid = selectedNumbers.filter((a, i) => a > selectedNumbers[i + 1]).length === 0;
+
+                if(isValid){
+                    setShowParentCaptcha(!isValid);
+                    setError(false);
+                }else{
+                    setError(true);
+                }
+
+                generateRandomNumbers();
+                return;
+            }
+        }
+
+    }, [ checkNumbers ]);
+
+    return(
+        <Grid
+            container
+            justifyContent={isUsedForEndClass ? `center` : `space-around`}
+            className={classes.parentChecker}>
+            {checkNumbers.map((number, index) => (
+                <Grid
+                    key={`numer-${index}`}
+                    item>
+                    <Typography
+                        variant="h4"
+                        className={clsx(classes.parentCheckerItem, {
+                            [classes.parentCheckerItemActive] : number.checked,
+                            [classes.smallChecker] : isUsedForEndClass,
+                            [classes.largeChecker] : !isUsedForEndClass,
+                        })}
+                        onClick={() => handleSelectNumber(number.value, index)} >{number.value}</Typography>
+                </Grid>
+            ))}
+        </Grid>
+    );
+}
+export { CaptchaLogic };
