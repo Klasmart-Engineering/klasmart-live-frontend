5a23ea6df7f714dad80c7992b35488febfb03be7
Hung Huynh <hung.huynh@kidsloop.live>
2021-12-29 07:01:06 +0000

    fix: improving observe in viewport feature
    Without use in-viewport feature on Present Mode, re-subscribe when switching lesson material
    
    
    Jira: KLL-2222
    Approved-by: Axel Wettervik
    Approved-by: Gaël Hellé Hellé
    Approved-by: Mamur Abdullaev

--- src/components/interactiveContent/previewPlayer.tsx
+++ src/components/interactiveContent/previewPlayer.tsx
@@ -105,7 +105,7 @@ export function PreviewPlayer ({
             streamId,
         },
     }, {
-        onlyObserveInViewport: isFeatureEnabled(features, FeatureFlag.ONLY_OBSERVE_IN_VIEWPORT),
+        onlyObserveInViewport: isFeatureEnabled(features, FeatureFlag.ONLY_OBSERVE_IN_VIEWPORT) && inViewport !== undefined,
         inViewport: !!inViewport
     });
 
