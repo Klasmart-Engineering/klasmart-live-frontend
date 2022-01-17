6debb5339800009d18c9d218c3af0fed03cabd00
Hang Nguyen Thuy <hangnguyent10@nashtechglobal.com>
2022-01-17 07:50:37 +0000

    feat: refactor alert popper in view mode menu
    * feat: refactor alert popper in view mode menu
    * refactor: create new config variable for alert time display and width
    
    
    Jira: KLL-2454, KLL-2040
    Approved-by: Henrik Wassdahl
    Approved-by: Axel Wettervik
    Approved-by: Nguyen Minh Duc
    Approved-by: Gaël Hellé Hellé

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,4 +1,3 @@
-import StyledIcon from "@/components/styled/icon";
 import {
     ContentType,
     InteractiveMode,
@@ -8,15 +7,12 @@ import { MaterialTypename } from "@/types/lessonMaterial";
 import {
     Drawer,
     Fade,
-    Grid,
     makeStyles,
     Paper,
     Popper,
     Theme,
     Typography,
 } from "@material-ui/core";
-import { grey } from "@material-ui/core/colors";
-import { ExclamationCircleFill as WarningIcon } from '@styled-icons/bootstrap/ExclamationCircleFill';
 import React from "react";
 import { useRecoilValue } from "recoil";
 
@@ -58,12 +54,6 @@ const useStyles = makeStyles((theme: Theme) => ({
         maxHeight: `calc(100vh - 150px)`,
         overflowY: `auto`,
         boxShadow: `0 1px 2px 0 rgba(0, 0, 0, 0.075), 0 2px 12px 0px rgba(0, 0, 0, 0.065)`,
-    },
-    alertPopper: {
-        backgroundColor: grey[100],
-        color: grey[700],
-        padding: theme.spacing(1),
-        width: 411,
     },
 }));
 
@@ -151,67 +141,6 @@ function StyledPopper (props: StyledPopperProps) {
 }
 
 export { StyledPopper };
-
-interface AlertPopperProps {
-	title: React.ReactNode;
-	open?: boolean;
-	anchorEl?: HTMLElement;
-}
-
-function AlertPopper (props: AlertPopperProps) {
-    const classes = useStyles();
-    const {
-        title,
-        open = false,
-        anchorEl,
-    } = props;
-
-    return (
-        <Popper
-            transition
-            open={open}
-            anchorEl={anchorEl}
-            disablePortal={false}
-            placement="top"
-            modifiers={{
-                preventOverflow: {
-                    boundariesElement: document.getElementById(`main-content`),
-                },
-                offset: {
-                    enabled: true,
-                    offset: `10,10`,
-                },
-            }}
-            className={classes.popperRoot}
-        >
-            <Fade in={open}>
-                <Paper className={classes.popperPapper}>
-                    <Grid
-                        container
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="space-around"
-                        className={classes.alertPopper}
-                    >
-                        <Grid item>
-                            <StyledIcon
-                                icon={<WarningIcon />}
-                                size={`medium`}
-                                color={grey[400]}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <Typography>{title}</Typography>
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Fade>
-        </Popper>
-    );
-}
-
-export { AlertPopper };
-
 interface TabPanelProps {
 	children?: React.ReactNode;
 	index: any;
