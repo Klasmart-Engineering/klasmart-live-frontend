320b2dd26b40fcc4fa4144bc2b04730c7bbf0c04
Phat Ho Hoang <phat.hohoang@nashtechglobal.com>
2022-02-07 07:21:58 +0000

    feat: add x option to study activity
    Jira: KLL-2230
    Approved-by: Axel Wettervik
    Approved-by: Nguyen Minh Duc
    Approved-by: Gaël Hellé Hellé

--- src/app/dialogs/infoDialog.tsx
+++ src/app/dialogs/infoDialog.tsx
@@ -1,3 +1,10 @@
+import { CloseIconButton } from "../components/icons/closeIconButton";
+import { usePopupContext } from "../context-provider/popup-context";
+import { useDeviceOrientationValue } from "../model/appModel";
+import {
+    HOME_FUN_STUDY_SUBMIT_BUTTON_BACKGROUND_COLOR,
+    TEXT_COLOR_MENU_DRAWER,
+} from "@/config";
 import {
     Button,
     Dialog,
@@ -7,37 +14,53 @@ import {
     Grid,
     Typography,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import React from "react";
+import {
+    createStyles,
+    makeStyles,
+    Theme,
+} from "@material-ui/core/styles";
+import clsx from "clsx";
+import React,
+{ useEffect } from "react";
 import { FormattedMessage } from "react-intl";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => createStyles({
     rounded_dialog: {
         borderRadius: `12px`,
     },
+    dialogLandscapeMaxWidth: {
+        maxWidth: `50%`,
+    },
     dialogTitle: {
-        backgroundColor: `#3671CE`,
+        backgroundColor: HOME_FUN_STUDY_SUBMIT_BUTTON_BACKGROUND_COLOR,
     },
     dialogTitleText: {
-        color: `white`,
+        color: theme.palette.common.white,
+    },
+    dialogContent: {
+        paddingTop: theme.spacing(1),
     },
     buttonClose: {
-        color: `#3671CE`,
+        backgroundColor: HOME_FUN_STUDY_SUBMIT_BUTTON_BACKGROUND_COLOR,
+        color: theme.palette.common.white,
     },
-});
+}));
 
 interface Props {
     open: boolean;
-    onClose: () => void;
     title?: string;
     description: string[];
     closeLabel?: string;
+    showCloseIcon: boolean;
+    onClose: (reason?: "backdropClick" | "escapeKeyDown") => void;
 }
 
 export function InfoDialog ({
-    open, onClose, title, description, closeLabel,
+    open, onClose, title, description, closeLabel, showCloseIcon,
 }: Props): JSX.Element {
     const classes = useStyles();
+    const deviceOrientation = useDeviceOrientationValue();
+    const { closePopup } = usePopupContext();
 
     const handleCloseClick = () => {
         onClose();
@@ -50,18 +73,38 @@ export function InfoDialog ({
                 maxWidth={`sm`}
                 className={classes.rounded_dialog}
                 classes={{
-                    paper: classes.rounded_dialog,
+                    paper: clsx(classes.rounded_dialog, {
+                        [classes.dialogLandscapeMaxWidth]: deviceOrientation === `landscape-primary`
+                            || deviceOrientation === `landscape-secondary`,
+                    }),
                 }}
                 open={open}
-                onClose={onClose}>
+                onClose={(event: object, reason: "backdropClick" | "escapeKeyDown") => {
+                    onClose(reason);
+                }}>
                 <DialogTitle className={classes.dialogTitle}>
-                    <Typography className={classes.dialogTitleText}>
-                        {
-                            title? title : <FormattedMessage
-                                id="label_info"
-                                defaultMessage={`Info`}/>
-                        }
-                    </Typography>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        wrap="nowrap">
+                        <Grid item>
+                            <Typography className={classes.dialogTitleText}>
+                                {
+                                    title ? title : <FormattedMessage
+                                        id="label_info"
+                                        defaultMessage={`Info`} />
+                                }
+                            </Typography>
+                        </Grid>
+                        {showCloseIcon && <Grid item>
+                            <CloseIconButton
+                                buttonSize="small"
+                                color={TEXT_COLOR_MENU_DRAWER}
+                                onClick={() => closePopup()} />
+                        </Grid>}
+                    </Grid>
                 </DialogTitle>
                 <DialogContent>
                     <Grid
@@ -76,9 +119,12 @@ export function InfoDialog ({
                                     item
                                     xs>
                                     <Typography
-                                        component={"div"}
+                                        className={classes.dialogContent}
+                                        component={`div`}
                                         variant="body2"
-                                        color={`textSecondary`}><div dangerouslySetInnerHTML={{__html: item}}/></Typography>
+                                        color={`textSecondary`}><div dangerouslySetInnerHTML={{
+                                            __html: item,
+                                        }}/></Typography>
                                 </Grid>)
                         }
                     </Grid>
