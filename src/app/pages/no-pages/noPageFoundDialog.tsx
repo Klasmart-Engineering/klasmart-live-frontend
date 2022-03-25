8f6f2de0c968aa9adf0329826e92b80ad033d4f8
Hang Nguyen Thuy <hangnguyent10@nashtechglobal.com>
2022-03-25 03:53:41 +0000

    feat: replace design for no org screen
    * fix: disable button elevation
    * fix: custom font size for button
    
    
    Jira: KLL-3220
    Approved-by: Axel Wettervik
    Approved-by: Phu Nguyen
    Approved-by: Gaël Hellé Hellé

--- src/app/pages/no-pages/noPageFoundDialog.tsx
+++ src/app/pages/no-pages/noPageFoundDialog.tsx
@@ -3,14 +3,18 @@ import {
     isShowOnBoardingState,
     shouldClearCookieState,
 } from "@/app/model/appModel";
-import BackIcon from "@/assets/img/back_icon.svg";
-import { TEXT_COLOR_SECONDARY_DEFAULT } from "@/config";
+import {
+    BG_COLOR_SIGN_IN_BUTTON,
+    THEME_COLOR_BACKGROUND_ON_BOARDING,
+} from "@/config";
 import {
     Button,
     Dialog,
     Grid,
     Theme,
     Typography,
+    useMediaQuery,
+    useTheme,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import React from "react";
@@ -18,25 +22,52 @@ import { FormattedMessage } from "react-intl";
 import { useSetRecoilState } from "recoil";
 
 const useStyles = makeStyles((theme: Theme) => ({
+    fullWidth: {
+        width: `100%`,
+        textAlign: `center`,
+    },
     container: {
-        padding: theme.spacing(4),
-        paddingTop: theme.spacing(8),
+        padding: theme.spacing(0, 2, 6),
+        height: `100%`,
+        backgroundColor: THEME_COLOR_BACKGROUND_ON_BOARDING,
     },
     content: {
-        textAlign: `center`,
-        paddingTop: theme.spacing(3),
+        height: `60%`,
+        [theme.breakpoints.up(`sm`)]: {
+            height: `50%`,
+        },
     },
-    titleText: {
-        marginTop: theme.spacing(4),
-        marginBottom: theme.spacing(4),
+    bodyContainer: {
+        width: `85%`,
+    },
+    img: {
+        width: 170,
+        [theme.breakpoints.up(`sm`)]: {
+            width: 230,
+        },
+    },
+    title: {
         fontWeight: theme.typography.fontWeightBold as number,
-        color: TEXT_COLOR_SECONDARY_DEFAULT,
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(1.5),
+
+        [theme.breakpoints.down(`sm`)]: {
+            fontSize: `1.25rem`,
+        },
     },
-    goBackContainer: {
-        position: `absolute`,
-        bottom: theme.spacing(16),
-        left: 0,
-        right: 0,
+    goBackButton: {
+        maxWidth: 330,
+        width: `100%`,
+        fontSize: `1.15rem`,
+        lineHeight: 1.334,
+        fontWeight: theme.typography.fontWeightBold as number,
+        backgroundColor: THEME_COLOR_BACKGROUND_ON_BOARDING,
+        color: theme.palette.common.white,
+        padding: theme.spacing(2),
+        borderRadius: theme.spacing(3, 1, 3, 3),
+        "&:hover": {
+            backgroundColor: BG_COLOR_SIGN_IN_BUTTON,
+        },
     },
 }));
 
@@ -53,7 +84,9 @@ export function NoPageFoundDialog ({
 }: Props): JSX.Element {
     const { actions } = useAuthenticationContext();
     const classes = useStyles();
+    const theme = useTheme();
     const setShouldClearCookie = useSetRecoilState(shouldClearCookieState);
+    const isSmUp = useMediaQuery(theme.breakpoints.up(`sm`));
     const setShowOnBoarding = useSetRecoilState(isShowOnBoardingState);
     return (
         <Dialog
@@ -64,55 +97,52 @@ export function NoPageFoundDialog ({
                 container
                 className={classes.container}
                 direction="column"
-                justifyContent="center"
+                justifyContent={isSmUp ? `space-around` : `space-between`}
                 alignItems="center">
-                <Grid item>
-                    <img
-                        alt="No Organization Found Logo"
-                        src={imgSrc}
-                    />
-                </Grid>
-                <Grid item>
-                    <Typography
-                        className={classes.titleText}
-                        variant="h4">
-                        <FormattedMessage id={title} />
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <Typography
-                        variant="subtitle1"
-                        align="center"
-                        style={{
-                            color: TEXT_COLOR_SECONDARY_DEFAULT,
-                        }}>
-                        <FormattedMessage id={body} />
-                    </Typography>
+                <Grid
+                    container
+                    alignItems="center"
+                    direction="column"
+                    justifyContent="flex-end"
+                    className={classes.content}>
+                    <Grid item>
+                        <img
+                            className={classes.img}
+                            alt=""
+                            src={imgSrc}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Typography
+                            className={classes.title}
+                            variant="h4">
+                            <FormattedMessage id={title} />
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        item
+                        className={classes.bodyContainer}>
+                        <Typography
+                            variant="h5"
+                            align="center">
+                            <FormattedMessage id={body} />
+                        </Typography>
+                    </Grid>
                 </Grid>
                 <Grid
                     item
-                    style={{
-                        textAlign: `center`,
-                    }}>
-                    <div className={classes.goBackContainer}>
-                        <Button
-                            color="secondary"
-                            variant="contained"
-                            onClick={() => {
-                                actions?.signOut();
-                                setShouldClearCookie(true);
-                                setShowOnBoarding(true);
-                            }}>
-                            <img
-                                alt="No Organization Found Back"
-                                src={BackIcon}
-                                style={{
-                                    marginRight: 2,
-                                }}
-                            />
-                            <FormattedMessage id="common.goBack" />
-                        </Button>
-                    </div>
+                    className={classes.fullWidth}>
+                    <Button
+                        disableElevation
+                        className={classes.goBackButton}
+                        variant="contained"
+                        onClick={() => {
+                            actions?.signOut();
+                            setShouldClearCookie(true);
+                            setShowOnBoarding(true);
+                        }}>
+                        <FormattedMessage id="common.goBack" />
+                    </Button>
                 </Grid>
             </Grid>
         </Dialog>
