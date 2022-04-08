13a2750fb701271f53c3539b07e2397cbaf56e70
Hang Nguyen Thuy <100561910+hang-nguyen-thuy-nashtech@users.noreply.github.com>
2022-04-08 09:23:38 +0700

    feat: redesign loading page failed (#44)
    Jira: NAT-495
    
    * feat: redesign loading page failed
    
    * feat: change style width for image loading fail
    
    Co-authored-by: hang-nguyen-thuy-nashtech <hang.nguyenthuy10@nashtechglobal.com>

--- src/app/components/Schedule/ErrorRetryButton.tsx
+++ src/app/components/Schedule/ErrorRetryButton.tsx
@@ -1,21 +1,41 @@
-import LoadingError from "@/assets/img/error/2.png";
+import LoadingError from "@/assets/img/error/5.svg";
+import { BG_COLOR_SIGN_IN_BUTTON } from "@/config";
 import {
     Box,
     Button,
     createStyles,
     makeStyles,
+    Theme,
     Typography,
+    useMediaQuery,
+    useTheme,
 } from "@material-ui/core";
 import React from "react";
 import { FormattedMessage } from "react-intl";
 
-const useStyles = makeStyles((theme) => createStyles({
-    errorImage: {
-        width: 200,
-        height: 200,
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    img: {
+        width: `35%`,
+        height: `auto`,
+        maxWidth: 220,
     },
-    errorText: {
-        margin: theme.spacing(2, 0),
+    body: {
+        margin: theme.spacing(2, 0, 3),
+        fontWeight: theme.typography.fontWeightBold as number,
+    },
+    retryButton: {
+        fontSize: `1rem`,
+        lineHeight: 1.55,
+        color: theme.palette.common.white,
+        backgroundColor: BG_COLOR_SIGN_IN_BUTTON,
+        fontWeight: theme.typography.fontWeightBold as number,
+        padding: theme.spacing(1, 5.5),
+        borderRadius: theme.spacing(1.5),
+
+        [theme.breakpoints.up(`sm`)]: {
+            fontSize: `1.4rem`,
+            padding: theme.spacing(1, 7),
+        },
     },
 }));
 
@@ -26,6 +46,8 @@ interface Props {
 export default function ScheduleErrorRetryButton (props: Props) {
     const { onClick } = props;
     const classes = useStyles();
+    const theme = useTheme();
+    const isSmUp = useMediaQuery(theme.breakpoints.up(`sm`));
     return (
         <Box
             display="flex"
@@ -34,22 +56,34 @@ export default function ScheduleErrorRetryButton (props: Props) {
             p={4}
         >
             <img
+                alt=""
                 src={LoadingError}
-                className={classes.errorImage}
+                className={classes.img}
             />
             <Typography
-                variant="body2"
-                color="textSecondary"
+                variant={isSmUp ? `h4`: `h5`}
                 align="center"
-                className={classes.errorText}
+                className={classes.body}
             >
-                <FormattedMessage id="schedule_errorFetchTimeViews" />
+                <FormattedMessage
+                    values={{
+                        br: <br />,
+                    }}
+                    id="schedule_errorFetchTimeViews"
+                    defaultMessage="Oops!{br}Page loading failed."
+                />
             </Typography>
             <Button
+                disableElevation
                 size="large"
+                variant="contained"
+                className={classes.retryButton}
                 onClick={onClick}
             >
-                Retry
+                <FormattedMessage
+                    id="loading.error.retry"
+                    defaultMessage="Retry"
+                />
             </Button>
         </Box>
     );
